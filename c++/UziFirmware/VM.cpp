597a0d51be4adf1d9145f91f7a10fd4ed55ec19e
RichoM <richi.moran@gmail.com>
2014-12-15 18:39:10 -0300

    Using a cool trick learned from Casey Muratori in his handmadehero.org stream that allows me to declare variables inside a switch's case with the correct scope.

--- c++/UziFirmware/VM.cpp
+++ c++/UziFirmware/VM.cpp
@@ -44,85 +44,79 @@ void VM::executeBytecode(unsigned char bytecode) {
 	unsigned char key = bytecode & 0xF0;
 	unsigned char value = bytecode & 0x0F;
 	
-	float a;
-	float b;
-
 	switch (key) {
-		case 0x00:// pushLit
+		case 0x00: {// pushLit
 			_stack->push(_currentScript->literalAt(value));
-			break;
-		case 0x10:// pushLocal
-			break;
-		case 0x20:// pushGlobal
-			break;
-		case 0x30:// setLocal
-			break;
-		case 0x40:// setGlobal
-			break;
-		case 0x50:// primCall
+		} break;
+		case 0x10: {// pushLocal
+		} break;
+		case 0x20: {// pushGlobal
+		} break;
+		case 0x30: {// setLocal
+		} break;
+		case 0x40: {// setGlobal
+		} break;
+		case 0x50: {// primCall
 			executePrimitive(value);
-			break;
-		case 0x60:// scriptCall
-			break;
-		case 0x70:// pop
+		} break;
+		case 0x60: {// scriptCall
+		} break;
+		case 0x70: {// pop
 			for (int i = 0; i < value; i++) {
 				_stack->pop();
 			}
-			break;		
-		case 0x80:// jmp
+		} break;
+		case 0x80: {// jmp
 			_ip = _ip + value;
-			break;
-		case 0x90:// jne
-			a = _stack->pop();
-			b = _stack->pop();
+		} break;
+		case 0x90: {// jne
+			float a = _stack->pop();
+			float b = _stack->pop();
 			if (a != b) {
 				_ip = _ip + value;
 			}
-			break;
-		case 0xA0:// jnz
+		} break;
+		case 0xA0: {// jnz
 			if (_stack->pop() != 0) {
 				_ip = _ip + value;
 			}
-			break;
-		case 0xB0:
-			break;
-		case 0xC0:
-			break;
-		case 0xD0:
-			break;
-		case 0xE0:
-			break;
-		case 0xF0:// extend
-			break;
+		} break;
+		case 0xB0: {
+		} break;
+		case 0xC0: {
+		} break;
+		case 0xD0: {
+		} break;
+		case 0xE0: {
+		} break;
+		case 0xF0: {// extend
+		} break;
 	}
 }
 
 void VM::executePrimitive(unsigned char primitiveIndex) {
 
-	float pop1;
-	float pop2;
-
 	switch(primitiveIndex) {
-		case 0x00:// getValue
-			pop1 = _stack->pop();
-			_stack->push(_pe->getValue((unsigned int)pop1));
-			break;
-		case 0x01:// setValue
-			pop1 = _stack->pop();
-			pop2 = _stack->pop(); 
-			_pe->setValue((unsigned int)pop2, pop1);
-			break;
-		case 0x02:// getMode
-			pop1 = _stack->pop();
-			_stack->push(_pe->getMode((unsigned int)pop1));
-			break;
-		case 0x03:// setMode
-			pop1 = _stack->pop();
-			pop2 = _stack->pop();
-			_pe->setMode((unsigned int)pop2, (unsigned char)pop1);
-			break;
-		case 0x04:// delay
+		case 0x00: {// getValue
+			unsigned int pin = _stack->pop();
+			_stack->push(_pe->getValue(pin));
+		} break;
+		case 0x01: {// setValue
+			float value = _stack->pop();
+			unsigned int pin = _stack->pop(); 
+			_pe->setValue(pin, value);
+		} break;
+		case 0x02: {// getMode
+			unsigned int pin = _stack->pop();
+			_stack->push(_pe->getMode(pin));
+		} break;
+		case 0x03: {// setMode
+			unsigned char mode = _stack->pop();
+			unsigned int pin = _stack->pop();
+			_pe->setMode(pin, mode);
+		} break;
+		case 0x04: {// delay
 			_pe->delayMs(_stack->pop());
-			break;
+		} break;
 	}
 }
