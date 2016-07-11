a35481b8d0e950092bdb51b3df338d66c67da866
RichoM <richi.moran@gmail.com>
2016-07-11 20:18:34 -0300

    Added ASCII and HEX consoles.

--- c++/Simulator/Simulator/SerialConsole.Designer.cs
+++ c++/Simulator/Simulator/SerialConsole.Designer.cs
@@ -30,14 +30,21 @@
         {
             this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.inputTextBox = new System.Windows.Forms.TextBox();
             this.sendButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.asciiPage = new System.Windows.Forms.TabPage();
+            this.asciiTextBox = new System.Windows.Forms.TextBox();
+            this.hexPage = new System.Windows.Forms.TabPage();
+            this.hexTextBox = new System.Windows.Forms.TextBox();
             this.updateTimer = new System.Windows.Forms.Timer(this.components);
-            this.outputTextBox = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.asciiPage.SuspendLayout();
+            this.hexPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -54,10 +61,22 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.outputTextBox);
+            this.splitContainer1.Panel2.Controls.Add(this.tabControl1);
             this.splitContainer1.Size = new System.Drawing.Size(575, 494);
             this.splitContainer1.SplitterDistance = 99;
             this.splitContainer1.TabIndex = 0;
+            // 
+            // sendButton
+            // 
+            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sendButton.Location = new System.Drawing.Point(466, 3);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(106, 95);
+            this.sendButton.TabIndex = 1;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
             // 
             // inputTextBox
             // 
@@ -72,35 +91,66 @@
             this.inputTextBox.Size = new System.Drawing.Size(460, 95);
             this.inputTextBox.TabIndex = 0;
             // 
-            // sendButton
+            // tabControl1
             // 
-            this.sendButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.sendButton.Location = new System.Drawing.Point(466, 3);
-            this.sendButton.Name = "sendButton";
-            this.sendButton.Size = new System.Drawing.Size(106, 95);
-            this.sendButton.TabIndex = 1;
-            this.sendButton.Text = "Send";
-            this.sendButton.UseVisualStyleBackColor = true;
-            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            this.tabControl1.Controls.Add(this.asciiPage);
+            this.tabControl1.Controls.Add(this.hexPage);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(575, 391);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // asciiPage
+            // 
+            this.asciiPage.Controls.Add(this.asciiTextBox);
+            this.asciiPage.Location = new System.Drawing.Point(4, 22);
+            this.asciiPage.Name = "asciiPage";
+            this.asciiPage.Padding = new System.Windows.Forms.Padding(3);
+            this.asciiPage.Size = new System.Drawing.Size(567, 365);
+            this.asciiPage.TabIndex = 0;
+            this.asciiPage.Text = "ASCII";
+            this.asciiPage.UseVisualStyleBackColor = true;
+            // 
+            // asciiTextBox
+            // 
+            this.asciiTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.asciiTextBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.asciiTextBox.Location = new System.Drawing.Point(3, 3);
+            this.asciiTextBox.Multiline = true;
+            this.asciiTextBox.Name = "asciiTextBox";
+            this.asciiTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.asciiTextBox.Size = new System.Drawing.Size(561, 359);
+            this.asciiTextBox.TabIndex = 2;
+            // 
+            // hexPage
+            // 
+            this.hexPage.Controls.Add(this.hexTextBox);
+            this.hexPage.Location = new System.Drawing.Point(4, 22);
+            this.hexPage.Name = "hexPage";
+            this.hexPage.Padding = new System.Windows.Forms.Padding(3);
+            this.hexPage.Size = new System.Drawing.Size(567, 365);
+            this.hexPage.TabIndex = 1;
+            this.hexPage.Text = "HEX";
+            this.hexPage.UseVisualStyleBackColor = true;
+            // 
+            // hexTextBox
+            // 
+            this.hexTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.hexTextBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hexTextBox.Location = new System.Drawing.Point(3, 3);
+            this.hexTextBox.Multiline = true;
+            this.hexTextBox.Name = "hexTextBox";
+            this.hexTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.hexTextBox.Size = new System.Drawing.Size(561, 359);
+            this.hexTextBox.TabIndex = 3;
             // 
             // updateTimer
             // 
             this.updateTimer.Interval = 1;
             this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
-            // 
-            // outputTextBox
-            // 
-            this.outputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.outputTextBox.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.outputTextBox.Location = new System.Drawing.Point(0, 1);
-            this.outputTextBox.Multiline = true;
-            this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.outputTextBox.Size = new System.Drawing.Size(575, 390);
-            this.outputTextBox.TabIndex = 2;
             // 
             // SerialConsole
             // 
@@ -115,9 +165,13 @@
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
-            this.splitContainer1.Panel2.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.asciiPage.ResumeLayout(false);
+            this.asciiPage.PerformLayout();
+            this.hexPage.ResumeLayout(false);
+            this.hexPage.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -128,6 +182,10 @@
         private System.Windows.Forms.TextBox inputTextBox;
         private System.Windows.Forms.Button sendButton;
         private System.Windows.Forms.Timer updateTimer;
-        private System.Windows.Forms.TextBox outputTextBox;
+        private System.Windows.Forms.TextBox asciiTextBox;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage asciiPage;
+        private System.Windows.Forms.TabPage hexPage;
+        private System.Windows.Forms.TextBox hexTextBox;
     }
 }
--- c++/Simulator/Simulator/SerialConsole.cs
+++ c++/Simulator/Simulator/SerialConsole.cs
@@ -38,8 +38,13 @@ namespace Simulator
             Tuple<DateTime, string> output = sketch.ReadSerial();
 
             if (output == null) return;
-            outputTextBox.AppendText(string.Format("{0:HH:mm:ss.fff}| {1}", output.Item1, output.Item2));
-            outputTextBox.AppendText(Environment.NewLine);
+
+            asciiTextBox.AppendText(string.Format("{0:HH:mm:ss.fff} | {1}", output.Item1, output.Item2));
+            asciiTextBox.AppendText(Environment.NewLine);
+
+            string hex = string.Join(" ", Encoding.ASCII.GetBytes(output.Item2).Select(b => b.ToString("X2")));
+            hexTextBox.AppendText(string.Format("{0:HH:mm:ss.fff} | {1}", output.Item1, hex));
+            hexTextBox.AppendText(Environment.NewLine);
         }
     }
 }
