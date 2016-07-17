8a64beaf7cef896b7237fc49d864e309a29b0455
RichoM <richi.moran@gmail.com>
2016-07-17 19:16:25 -0300

    Temporarily changed default EEPROM values to store the default program.
    I'm using this to quickly test if the EEPROMReader is working.

--- c++/Simulator/Sketch/EEPROM.h
+++ c++/Simulator/Sketch/EEPROM.h
@@ -9,6 +9,14 @@ public:
 	EEPROMClass(void)
 	{
 		for (int i = 0; i < E2END + 1; i++) { bytes[i] = 255; }
+		// HACK(Richo): To test if the EEPROMReader is working, I initialize the EEPROM with the default program
+		unsigned char defaultProgram[] = {
+			0xC3,
+			2,
+			128, 0, 0, 0, 2, 8, 11, 14, 0, 0, 1, 80, 81, 255,
+			128, 0, 3, 232, 3, 12, 0, 1, 13, 0, 2, 80, 164, 2, 1, 81, 131, 2, 0, 81, 255
+		};
+		for (int i = 0; i < 37; i++) { bytes[i] = defaultProgram[i]; }
 	}
 
 	unsigned char read(int);
