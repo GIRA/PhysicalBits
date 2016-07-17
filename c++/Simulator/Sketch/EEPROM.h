dff81911af50df64e417fb49e75757aa06928e0d
RichoM <richi.moran@gmail.com>
2016-07-17 11:26:17 -0300

    Changed EEPROM constructor to initialize all its bytes to 255.

--- c++/Simulator/Sketch/EEPROM.h
+++ c++/Simulator/Sketch/EEPROM.h
@@ -6,11 +6,16 @@ class EEPROMClass
 {
 
 public:
+	EEPROMClass(void)
+	{
+		for (int i = 0; i < E2END + 1; i++) { bytes[i] = 255; }
+	}
+
 	unsigned char read(int);
 	void write(int, unsigned char);
 
 private:
-	unsigned char bytes[E2END + 1] = { 255 };
+	unsigned char bytes[E2END + 1];
 
 };
 
