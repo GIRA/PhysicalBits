c3f7b215c796e1bc99c0f3eaddd30d4c173fcb34
RichoM <richi.moran@gmail.com>
2016-07-17 20:02:16 -0300

    Changed EEPROM hack to use the new format for the default program.

--- c++/Simulator/Sketch/EEPROM.h
+++ c++/Simulator/Sketch/EEPROM.h
@@ -10,13 +10,16 @@ public:
 	{
 		for (int i = 0; i < E2END + 1; i++) { bytes[i] = 255; }
 		// HACK(Richo): To test if the EEPROMReader is working, I initialize the EEPROM with the default program
-		unsigned char defaultProgram[] = {
-			0xC3,
-			2,
-			128, 0, 0, 0, 2, 8, 11, 14, 0, 0, 1, 80, 81, 255,
-			128, 0, 3, 232, 3, 12, 0, 1, 13, 0, 2, 80, 164, 2, 1, 81, 131, 2, 0, 81, 255
+		unsigned char defaultProgram[] = { 
+			0, 
+			206, // BEGIN
+			195, // PROGRAM_START
+			2, 
+			128, 0, 0, 0, 2, 8, 11, 14, 0, 0, 1, 80, 81, 255, 
+			128, 0, 3, 232, 3, 12, 0, 1, 13, 0, 2, 80, 164, 2, 1, 81, 131, 2, 0, 81, 255, 
+			207 // END
 		};
-		for (int i = 0; i < 37; i++) { bytes[i] = defaultProgram[i]; }
+		for (int i = 0; i < 40; i++) { bytes[i] = defaultProgram[i]; }
 	}
 
 	unsigned char read(int);
