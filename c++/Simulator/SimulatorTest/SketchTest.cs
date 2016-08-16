12ece8ae9d9d27de41b62f54aeacd4d0ac18a797
RichoM <richi.moran@gmail.com>
2016-08-16 11:37:38 -0300

    Added test project.

---
+++ c++/Simulator/SimulatorTest/SketchTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simulator;
+
+namespace SimulatorTest
+{
+    [TestClass]
+    public class SketchTest
+    {
+        private Sketch sketch = Sketch.Current;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            sketch.Setup();
+            TurnOffAllPins();
+        }
+
+        [TestMethod]
+        public void TestTurnOnBytecode()
+        {
+            sketch.WriteSerial(new byte[]
+            {
+                /*
+                Uzi program: [:p | p
+	                script: #test
+	                ticking: true
+	                delay: 0
+	                bytecodes: [:s | s
+		                turnOn: 13]].
+                */
+                0, 1, 0, 128, 0, 0, 0, 1, 13
+            });
+            Assert.AreEqual(0, sketch.GetPinValue(13));
+            sketch.Loop();
+            Assert.AreEqual(1023, sketch.GetPinValue(13));
+        }
+
+        [TestMethod]
+        public void TestTurnOffBytecode()
+        {
+            sketch.WriteSerial(new byte[]
+            {
+                /*
+                Uzi program: [:p | p
+	                script: #test
+	                ticking: true
+	                delay: 0
+	                bytecodes: [:s | s
+		                turnOff: 13]].
+                */
+                0, 1, 0, 128, 0, 0, 0, 1, 45
+            });
+            sketch.SetPinValue(13, 1023);
+            Assert.AreEqual(1023, sketch.GetPinValue(13));
+            sketch.Loop();
+            Assert.AreEqual(0, sketch.GetPinValue(13));
+        }
+
+        private void TurnOffAllPins()
+        {
+            for (int i = 0; i < 19; i++)
+            {
+                sketch.SetPinValue(i, 0);
+            }
+        }
+    }
+}
