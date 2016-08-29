974b172ec31a55003e279839358bc85e3347825e
RichoM <richi.moran@gmail.com>
2016-08-29 11:59:29 -0300

    Added types.h, which only has a couple of typedefs for integer types.
    I got tired of writing "unsigned" all the time, but I also don't like the
    "_t" suffix of stdint.h

---
+++ c++/UziFirmware/types.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdint.h>
+
+typedef int8_t int8;
+typedef int16_t int16;
+typedef int32_t int32;
+
+typedef uint8_t uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
