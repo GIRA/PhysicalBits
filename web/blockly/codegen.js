4b9343b35bc5db4873456cb13066f6e38bc0625c
RichoM <richi.moran@gmail.com>
2018-08-01 09:37:57 -0300

    Still adding code generation for more blocks.

--- web/blockly/codegen.js
+++ web/blockly/codegen.js
@@ -278,6 +278,332 @@ var CodeGenerator = (function () {
 				arguments: [pinNumber],
 				primitiveName: "toggle"
 			};
+		},
+		logic_operation: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "OP").innerText;
+			var left = generateCodeForValue(block, path, "A");
+			var right = generateCodeForValue(block, path, "B");
+			var selector, primName;
+			if (type === "AND") {
+				selector = "&&";
+				primName = "logicalAnd";
+			} else if (type === "OR") {
+				selector = "||";
+				primName = "logicalOr";
+			}
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: selector,
+				arguments: [left, right],
+				primitiveName: primName
+			};
+		},
+		logic_boolean: function (block, path) {
+			var id = getId(block);
+			var bool = getChildNode(block, "BOOL").innerText;			
+			return {
+				type: "UziNumberNode",
+				id: id,
+				value: bool === "TRUE" ? 1 : 0
+			};
+		},
+		logic_negate: function (block, path) {
+			var id = getId(block);
+			var bool = generateCodeForValue(block, path, "BOOL");
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: "not",
+				arguments: [bool],
+				primitiveName: "negate"
+			};
+		},
+		math_number_property: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "PROPERTY").innerText;
+			var num = generateCodeForValue(block, path, "NUMBER_TO_CHECK");
+			var selector, primName;
+			if (type === "EVEN") {
+				selector = "isEven";
+				primName = "isEven";
+			} else if (type === "ODD") {
+				selector = "isOdd";
+				primName = "isOdd";
+			} else if (type === "PRIME") {
+				selector = "isPrime";
+				primName = "isPrime";
+			} else if (type === "WHOLE") {
+				selector = "isWhole";
+				primName = "isWhole";
+			} else if (type === "POSITIVE") {
+				selector = "isPositive";
+				primName = "isPositive";
+			} else if (type === "NEGATIVE") {
+				selector = "isNegative";
+				primName = "isNegative";
+			} else {
+				throw "Math number property not found: '" + type + "'";
+			}
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: "not",
+				arguments: [num],
+				primitiveName: "negate"
+			};
+		},
+		repeat_times: function (block, path) {
+			var id = getId(block);
+			var times = generateCodeForValue(block, path, "times");
+			var statements = generateCodeForStatements(block, path);
+			return {
+				type: "UziRepeatNode",
+				id: id,
+				times: times,
+				body: {
+					type: "UziBlockNode",
+					id: id,
+					statements: statements
+				}
+			};
+		},
+		math_round: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "OP").innerText;
+			var num = generateCodeForValue(block, path, "NUM");
+			var selector, primName;
+			if (type === "ROUND") {
+				selector = "round";
+				primName = "round";
+			} else if (type === "ROUNDUP") {
+				selector = "ceil";
+				primName = "ceil";
+			} else if (type === "ROUNDDOWN") {
+				selector = "floor";
+				primName = "floor";
+			} else {
+				throw "Math round type not found: '" + type + "'";
+			}
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: selector,
+				arguments: [num],
+				primitiveName: primName
+			};
+		},
+		math_single: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "OP").innerText;
+			var num = generateCodeForValue(block, path, "NUM");
+			var selector, primName;
+			if (type === "ROOT") {
+				selector = "sqrt";
+				primName = "sqrt";
+			} else if (type === "ABS") {
+				selector = "abs";
+				primName = "abs";
+			} else if (type === "NEG") {
+				selector = "negate";
+				primName = "negate";
+			} else if (type === "LN") {
+				selector = "ln";
+				primName = "ln";
+			} else if (type === "LOG10") {
+				selector = "log10";
+				primName = "log10";
+			} else if (type === "EXP") {
+				selector = "exp";
+				primName = "exp";
+			} else if (type === "POW10") {
+				selector = "pow10";
+				primName = "pow10";
+			} else {
+				throw "Math function not found: '" + type + "'";
+			}
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: selector,
+				arguments: [num],
+				primitiveName: primName
+			};
+		},
+		math_trig: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "OP").innerText;
+			var num = generateCodeForValue(block, path, "NUM");
+			var selector, primName;
+			if (type === "SIN") {
+				selector = "sin";
+				primName = "sin";
+			} else if (type === "COS") {
+				selector = "cos";
+				primName = "cos";
+			} else if (type === "TAN") {
+				selector = "tan";
+				primName = "tan";
+			} else if (type === "ASIN") {
+				selector = "asin";
+				primName = "asin";
+			} else if (type === "ACOS") {
+				selector = "acos";
+				primName = "acos";
+			} else if (type === "ATAN") {
+				selector = "atan";
+				primName = "atan";
+			} else {
+				throw "Math trig function not found: '" + type + "'";
+			}
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: selector,
+				arguments: [num],
+				primitiveName: primName
+			};
+		},
+		math_constant: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "CONSTANT").innerText;
+			var value;
+			if (type === "PI") {
+				value = Math.PI;
+			} else if (type === "E") {
+				value = Math.E;
+			} else if (type === "GOLDEN_RATIO") {
+				value = 1.61803398875;
+			} else if (type === "SQRT2") {
+				value = Math.SQRT2;
+			} else if (type === "SQRT1_2") {
+				value = Math.SQRT1_2;
+			} else if (type === "INFINITY") {
+				value = Infinity;
+			} else {
+				throw "Math constant not found: '" + type + "'";
+			}
+			return {
+				type: "UziNumberNode",
+				id: id,
+				value: value
+			};
+		},
+		math_arithmetic: function (block, path) {
+			var id = getId(block);
+			var type = getChildNode(block, "OP").innerText;
+			var left = generateCodeForValue(block, path, "A");
+			var right = generateCodeForValue(block, path, "B");
+			var selector, primName;
+			if (type === "DIVIDE") {
+				selector = "/";
+				primName = "divide";
+			} else if (type === "MULTIPLY") {
+				selector = "*";
+				primName = "multiply";
+			} else if (type === "MINUS") {
+				selector = "-";
+				primName = "subtract";
+			} else if (type === "ADD") {
+				selector = "+";
+				primName = "add";
+			} else if (type === "POWER") {
+				selector = "**";
+				primName = "power";
+			} else {
+				throw "Math arithmetic function not found: '" + type + "'";
+			}
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: selector,
+				arguments: [left, right],
+				primitiveName: primName
+			};
+		},
+		timer: function (block, path) {
+			var id = getId(block);
+			var name = getChildNode(block, "taskName").innerText;
+			var runningTimes = parseInt(getChildNode(block, "runningTimes").innerText);
+			var tickingScale = getChildNode(block, "tickingScale").innerText;
+			var initialState = getChildNode(block, "initialState").innerText;
+			var statements = generateCodeForStatements(block, path);			
+			return {
+				type: "UziTaskNode",
+				id: id,
+				name: taskName,
+				arguments: [],
+				state: initialState === "started" ? "running" : "stopped",
+				tickingRate: {
+					type: "UziTickingRateNode",
+					id: id,
+					value: runningTimes,
+					scale: tickingScale
+				},
+				body: {
+					type: "UziBlockNode",
+					id: id,
+					statements: statements					
+				}
+			};
+		},
+		write_pin_variable: function (block, path) {
+			var id = getId(block);
+			var pinNumber = generateCodeForValue(block, path, "pinNumber");
+			var pinValue = generateCodeForValue(block, path, "pinValue");
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: "write",
+				arguments: [pinNumber, pinValue],
+				primitiveName: "write"
+			}
+		},
+		read_pin_variable: function (block, path) {
+			var id = getId(block);
+			var pinNumber = generateCodeForValue(block, path, "pinNumber");
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: "read",
+				arguments: [pinNumber],
+				primitiveName: "read"
+			};
+		},
+		degrees_servo_variable: function (block, path) {
+			var id = getId(block);
+			var pinNumber = generateCodeForValue(block, path, "pinNumber");
+			var servoValue = generateCodeForValue(block, path, "servoValue");
+			return {
+				type: "UziPrimitiveCallNode",
+				id: id,
+				selector: "servoDegrees",
+				arguments: [pinNumber, servoValue],
+				primitiveName: "servoDegrees"
+			};
+		},
+		repeat: function (block, path) {
+			var id = getId(block);
+			var negated = getChildNode(block, "negate").innerText === "true";
+			var condition = generateCodeForValue(block, path, "condition");
+			var statements = generateCodeForStatements(block, path);
+			return {
+				type: "UziWhileNode",
+				id: id,
+				pre: {
+					type: "UziBlockNode",
+					id: id,
+					statements: []
+				},
+				condition: condition,
+				post: {
+					type: "UziBlockNode",
+					id: id,
+					statements: statements
+				},
+				negated: negated
+			};
 		}
 	};
 	
