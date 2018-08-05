a16d894fa58a655a2ce30f5dadc4f4943cf741dc
RichoM <richi.moran@gmail.com>
2018-08-05 12:53:57 -0300

    Added tests for asin, acos, and atan primitives.

--- c++/Simulator/SimulatorTest/SketchTest.cs
+++ c++/Simulator/SimulatorTest/SketchTest.cs
@@ -1324,5 +1324,44 @@ namespace SimulatorTest
             Assert.AreEqual(0, sketch.GetPinValue(11), "D11 should be off");
             Assert.AreEqual(1023, sketch.GetPinValue(10), "D10 should be on");
         }
+
+        [TestMethod]
+        public void Test066Asin()
+        {
+            sketch.WriteSerial(ReadFile(nameof(Test066Asin)));
+
+            sketch.SetMillis(1000);
+            sketch.Loop();
+
+            Assert.AreEqual(0, sketch.GetPinValue(7), "D7 should be off");
+            Assert.IsTrue(Math.Abs(sketch.GetPinValue(8) - 512) < 5, "D8 should be close to 512");
+            Assert.AreEqual(1023, sketch.GetPinValue(9), "D9 should be on");
+        }
+
+        [TestMethod]
+        public void Test067Acos()
+        {
+            sketch.WriteSerial(ReadFile(nameof(Test067Acos)));
+
+            sketch.SetMillis(1000);
+            sketch.Loop();
+
+            Assert.AreEqual(0, sketch.GetPinValue(7), "D7 should be off");
+            Assert.IsTrue(Math.Abs(sketch.GetPinValue(8) - 512) < 5, "D8 should be close to 512");
+            Assert.AreEqual(1023, sketch.GetPinValue(9), "D9 should be on");
+        }
+
+        [TestMethod]
+        public void Test068Atan()
+        {
+            sketch.WriteSerial(ReadFile(nameof(Test068Atan)));
+
+            sketch.SetMillis(1000);
+            sketch.Loop();
+
+            Assert.AreEqual(0, sketch.GetPinValue(7), "D7 should be off");
+            Assert.IsTrue(Math.Abs(sketch.GetPinValue(8) - 512) < 5, "D8 should be close to 512");
+            Assert.AreEqual(1023, sketch.GetPinValue(9), "D9 should be on");
+        }
     }
 }
