fd8b77c0ad14f88f83e26fbf9600623a9c1e5f5d
RichoM <richi.moran@gmail.com>
2018-08-07 12:13:10 -0300

    Changed monitoring demo to allow the user to choose which pins to monitor.

--- web/monitoring/monitor.js
+++ web/monitoring/monitor.js
@@ -1,9 +1,36 @@
+$("#addGraphButton").on("click", function () {
+	choosePins(function (selection) {
+		console.log(selection);
+		for (var i = 0; i < selection.length; i++) {
+			Uzi.togglePinReport(selection[i]);
+		}
+	});	
+});
+
+function choosePins(callback) {
+	$("#monitorSelectionModal #acceptButton").on("click", function () {
+		var selected = [];
+		$("#monitorSelectionModal input:checked").each(function () { 
+			selected.push(parseInt(this.value));
+		});
+		callback(selected);
+		$("#monitorSelectionModal").modal('hide');
+	});
+	$("#monitorSelectionModal").on("hide.bs.modal", function () {
+		$("#monitorSelectionModal #acceptButton").off("click");
+		$("#monitorSelectionModal").off("hide.bs.modal");
+	});
+	$("#monitorSelectionModal input:checkbox").each(function () { 
+		this.checked = false; 
+	});
+	$("#monitorSelectionModal").modal({});
+}
 
 Uzi.onMonitorUpdate(function () {	
 	var str = "";
 	Uzi.pins.forEach(function (pin) {
 		str += "PIN " + pin.number;
-		pin.history.forEach(function (each) {
+		pin.history.slice(-10).forEach(function (each) {
 			str += "<br>\t" + each.timestamp + " -> " + each.value;
 		});
 		str += "<br><br>";
