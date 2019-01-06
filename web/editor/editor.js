2fa23bacd4a804aeaf4f390d975565a37c9c2c93
RichoM <richi.moran@gmail.com>
2019-01-06 01:01:54 -0300

    Implemented breakpoints in the ace editor. Just the UI, for now.

--- web/editor/editor.js
+++ web/editor/editor.js
@@ -1,4 +1,7 @@
 (function () {	
+
+	var breakpoints = [];
+
 	$("#compile").on("click", function () {
 		Uzi.compile(editor.getValue(), "text", function (bytecodes) {			
 			console.log(bytecodes);
@@ -21,6 +24,37 @@
 	
 	$("#debug").on("click", function() {
 		Alert.danger("Debugger not implemented yet");
+	});
+	
+	$(".ace_gutter").on("click", function (e) { 
+		var line = Number.parseInt(e.target.innerText) - 1;
+		if (breakpoints.includes(line)) {
+			var index = breakpoints.indexOf(line);
+			if (index > -1) { breakpoints.splice(index, 1); }
+			editor.session.clearBreakpoint(line);
+		} else {
+			breakpoints.push(line);
+			editor.session.setBreakpoint(line, "breakpoint");
+		}
+	});
+	
+	editor.on("change", function (e) { 
+		let start = e.start.row;
+		let delta = e.lines.length - 1;
+		if (e.action == "insert") {
+			delta *= 1;
+		} else if (e.action == "remove") {
+			delta *= -1;
+		} else {
+			debugger;
+		}
+		let bpts = breakpoints.filter(function (bp) { return bp > start; });
+		breakpoints = breakpoints.filter(function (bp) { return bp <= start; });
+		bpts.forEach(function (bp) { breakpoints.push(bp + delta); });
+		editor.session.clearBreakpoints();
+		breakpoints.forEach(function (line) {
+			editor.session.setBreakpoint(line, "breakpoint");
+		});
 	});
 
 	Uzi.onConnectionUpdate(function () {
