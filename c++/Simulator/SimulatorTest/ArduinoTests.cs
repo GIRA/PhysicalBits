0b9a199b8d045ceb6c7383517c5ec74e5dc6fa87
Matias Teragni <matias.teragni@gmail.com>
2019-02-04 13:02:58 -0300

    Added a test class to handle the physical validations of UZI. Added a test method that is not complete,  for now it just triggers the recording of the outputs

---
+++ c++/Simulator/SimulatorTest/ArduinoTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Simulator;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimulatorTest
+{
+    [TestClass]
+    public class ArduinoTests
+    {
+
+        private static readonly string TEST_FILES_PATH = Path.Combine("..", "SimulatorTest", "TestFiles");
+        private byte[] ReadFile(string fileName)
+        {
+            var str = File.ReadAllText(Path.Combine(TEST_FILES_PATH, fileName));
+            return str.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(each => byte.Parse(each))
+                .ToArray();
+        }
+
+
+        [TestMethod]
+        public void Test009TickingRate()
+        {
+            var execution = SketchRecorder.RecordExecution(ReadFile(nameof(Test009TickingRate)), 4000).ToList();
+
+        }
+
+    }
+}
