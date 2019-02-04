c811d4b3120d9a40ea17b71f60e65ebbeee9c3f4
Matias Teragni <matias.teragni@gmail.com>
2019-02-04 16:43:41 -0300

    Removed pins and added a toString to easily visualize a snapshot

--- c++/Simulator/Simulator/ExecutionSnapshot.cs
+++ c++/Simulator/Simulator/ExecutionSnapshot.cs
@@ -6,12 +6,17 @@ namespace Simulator
     public class ExecutionSnapshot
     {
         public int ms;
-        public byte[] pins = new byte[14];
+        public byte[] pins = new byte[4];
         public byte error;
 
         public bool IsDifferentThan(ExecutionSnapshot other)
         {
             return other == null || !pins.SequenceEqual(other.pins);
+        }
+
+        public override string ToString()
+        {
+            return ms + "," + string.Join(",", pins);
         }
     }
 }
