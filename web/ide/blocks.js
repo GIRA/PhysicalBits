e482a150989ecaeeff6ab05a7e4ff297da524150
RichoM <richi.moran@gmail.com>
2019-12-06 12:52:56 -0300

    Added polyfill for flat() function

--- web/ide/blocks.js
+++ web/ide/blocks.js
@@ -11,6 +11,17 @@ let UziBlock = (function () {
     })();
   }
 
+  // HACK(Richo): flat polyfill
+  if (!Array.prototype.flat) {
+    Object.defineProperty(Array.prototype, 'flat', {
+      value: function(depth = 1) {
+        return this.reduce(function (flat, toFlatten) {
+          return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+        }, []);
+      }
+    });
+  }
+
   let version = 1;
   let blocklyArea, blocklyDiv, workspace;
   let motors = [];
