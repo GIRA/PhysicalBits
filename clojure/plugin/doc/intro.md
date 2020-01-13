6b9d4c90dd0ea1d217eb9b530c4f8416625cc152
Ricardo Moran <richi.moran@gmail.com>
2020-01-12 22:10:23 -0300

    Added empty project.

---
+++ clojure/plugin/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to plugin
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/plugin/test/plugin/core_test.clj
@@ -0,0 +1,7 @@
+(ns plugin.core-test
+  (:require [clojure.test :refer :all]
+            [plugin.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
