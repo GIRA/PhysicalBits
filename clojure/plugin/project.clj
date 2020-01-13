e4ac0d57d195e562dcb0b49e1d35712f093b4e4f
Ricardo Moran <richi.moran@gmail.com>
2020-01-12 23:07:05 -0300

    Added sound notification when tests are run

--- clojure/plugin/project.clj
+++ clojure/plugin/project.clj
@@ -3,12 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.6.532"]]
   :main ^:skip-aot plugin.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
+             :test {:resource-paths ["env/test/resources"]}
              :dev {:dependencies [[org.clojure/tools.namespace "0.3.1"]
-                                  [pjstadig/humane-test-output "0.10.0"]]
+                                  [pjstadig/humane-test-output "0.10.0"]
+                                  [org.clojars.beppu/clj-audio "0.3.0"]]
                    :source-paths ["env/dev/clj"]
                    :resource-paths ["env/dev/resources"]
                    :repl-options {:init-ns user
