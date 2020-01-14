27fc20a199fde85dd53b84fc8e10d5cd090f2bf5
RichoM <richi.moran@gmail.com>
2020-01-14 10:16:07 -0300

    Added a few useful functions to the user namespace

--- clojure/plugin/env/dev/clj/user.clj
+++ clojure/plugin/env/dev/clj/user.clj
@@ -1,4 +1,25 @@
 (ns user
   (:require [plugin.core :refer :all]
+            [clojure.core.async :refer [go-loop <! <!! timeout]]
             [clojure.tools.namespace.repl :as repl])
   (:use [clojure.repl]))
+
+(defn reload []
+  (disconnect)
+  (repl/refresh))
+
+(defn millis [] (System/currentTimeMillis))
+
+(defn print-a0
+  ([] (print-a0 5000 10))
+  ([ms] (print-a0 ms 10))
+  ([ms interval]
+   (let [begin (millis)
+         end (+ begin ms)]
+     (time
+      (<!! (go-loop []
+             (let [now (millis)]
+               (when (< now end)
+                 (println (- now begin) ":" (@state :a0))
+                 (<! (timeout interval))
+                 (recur)))))))))
