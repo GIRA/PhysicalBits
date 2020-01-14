85a616583e8d71b4967ff9a83a31e924bdc8ac6e
Ricardo Moran <richi.moran@gmail.com>
2020-01-13 23:55:46 -0300

    Testing the serial port

--- clojure/plugin/project.clj
+++ clojure/plugin/project.clj
@@ -4,7 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.async "0.6.532"]]
+                 [org.clojure/core.async "0.6.532"]
+                 [clj-serial "2.0.5"]]
   :main ^:skip-aot plugin.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
--- clojure/plugin/src/plugin/core.clj
+++ clojure/plugin/src/plugin/core.clj
@@ -1,5 +1,35 @@
 (ns plugin.core
+  (:require [serial.core :as s]
+            [serial.util :refer :all])
   (:gen-class))
+
+(def state (atom {:port nil}))
+
+(defn- error [msg] (println "ERROR:" msg))
+
+(defn connected? []
+  (not (nil? (@state :port))))
+
+(defmacro check-connection [then-form]
+  `(if (connected?)
+     ~then-form
+     (error "The board is not connected!")))
+
+(defn- process-input [input]
+  (println "I:" (.read input)))
+
+(defn connect [port-name baud-rate]
+  (if (connected?)
+    (error "The board is already connected")
+    (let [port (s/open port-name :baud-rate baud-rate)]
+      (s/listen! port process-input)
+      (reset! state {:port port}))))
+
+(defn disconnect []
+  (check-connection
+   (let [port (@state :port)]
+     (reset! state {:port nil})
+     (s/close! port))))
 
 (defn -main
   "I don't do a whole lot ... yet."
