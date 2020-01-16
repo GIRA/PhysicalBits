cf70f9da9d10befa641b986f204b8f6d757ee601
RichoM <richi.moran@gmail.com>
2020-01-16 09:23:02 -0300

    Uzing memoize to optimize the plugin.boards functions

--- clojure/plugin/src/plugin/boards.clj
+++ clojure/plugin/src/plugin/boards.clj
@@ -6,16 +6,18 @@
          :pin-numbers [2 3 4 5 6 7 8 9 10 11 12 13
                        14 15 16 17 18 19]}})
 
-(defn get-pin-name [pin-number]
-  (let [board (boards :uno) ; TODO(Richo): Hardcoded UNO board!
-        index (.indexOf (board :pin-numbers) pin-number)]
-    (if-not (= -1 index)
-      (nth (board :pin-names) index)
-      nil)))
+(def get-pin-name
+  (memoize (fn [pin-number]
+             (let [board (boards :uno) ; TODO(Richo): Hardcoded UNO board!
+                   index (.indexOf (board :pin-numbers) pin-number)]
+               (if-not (= -1 index)
+                 (nth (board :pin-names) index)
+                 nil)))))
 
-(defn get-pin-number [pin-name]
-  (let [board (boards :uno) ; TODO(Richo): Hardcoded UNO board!
-        index (.indexOf (board :pin-names) pin-name)]
-    (if-not (= -1 index)
-      (nth (board :pin-numbers) index)
-      nil)))
+(def get-pin-number
+  (memoize (fn [pin-name]
+             (let [board (boards :uno) ; TODO(Richo): Hardcoded UNO board!
+                   index (.indexOf (board :pin-names) pin-name)]
+               (if-not (= -1 index)
+                 (nth (board :pin-numbers) index)
+                 nil)))))
