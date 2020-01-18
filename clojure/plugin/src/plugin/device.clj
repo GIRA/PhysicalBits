1130e1eab248a793b5b24f99f17c066f579e8e08
Ricardo Moran <richi.moran@gmail.com>
2020-01-18 20:41:53 -0300

    A little refactoring

--- clojure/plugin/src/plugin/device.clj
+++ clojure/plugin/src/plugin/device.clj
@@ -89,16 +89,27 @@
       (<! (timeout 100))
       (recur))))
 
-(defn- read-timestamp [in]
+(defn- bytes->uint32 [[n1 n2 n3 n4]]
+  (bit-or (bit-shift-left n1 24)
+          (bit-shift-left n2 16)
+          (bit-shift-left n3 8)
+          n4))
+
+(defn- uint32->float [uint32]
+  (Float/intBitsToFloat (unchecked-int uint32)))
+
+(defn- bytes->float [bytes]
+  (uint32->float (bytes->uint32 bytes)))
+
+(defn- read-uint32 [in]
   (go
    (let [n1 (<? in)
          n2 (<? in)
          n3 (<? in)
          n4 (<? in)]
-     (bit-or (bit-shift-left n1 24)
-             (bit-shift-left n2 16)
-             (bit-shift-left n3 8)
-             n4))))
+     (bytes->uint32 [n1 n2 n3 n4]))))
+
+(def read-timestamp read-uint32)
 
 (defn- process-pin-value [in]
   (go
@@ -129,12 +140,7 @@
                     n2 (<? in)
                     n3 (<? in)
                     n4 (<? in)
-                    float-value (Float/intBitsToFloat
-                                 (unchecked-int
-                                  (bit-or (bit-shift-left n1 24)
-                                          (bit-shift-left n2 16)
-                                          (bit-shift-left n3 8)
-                                          n4)))]
+                    float-value (bytes->float [n1 n2 n3 n4])]
                 (swap! state update-in [:globals number]
                        (fn [_] {:name "?"
                                 :number number
