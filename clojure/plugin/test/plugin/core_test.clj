2a7e1af5d5f3094f408297ef6415810f963fb205
RichoM <richi.moran@gmail.com>
2020-02-03 12:11:52 -0300

    Renamed default test

--- clojure/plugin/test/plugin/core_test.clj
+++ clojure/plugin/test/plugin/core_test.clj
@@ -2,6 +2,6 @@
   (:require [clojure.test :refer :all]
             [plugin.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest sanity-check
+  (testing "Sanity check."
+    (is (= 1 1))))
