318933cbfcdf61842e7b28b543cd5818f47d5020
RichoM <richi.moran@gmail.com>
2020-02-03 12:10:55 -0300

    Added first compiler test

---
+++ clojure/plugin/test/plugin/compiler_test.clj
@@ -0,0 +1,31 @@
+(ns plugin.compiler-test
+  (:require [clojure.test :refer :all]
+            [plugin.compiler :refer :all]))
+
+
+(deftest empty-program-test
+  (let [ast {:__class__ "UziProgramNode",
+             :scripts [{:__class__ "UziTaskNode",
+                        :name "empty",
+                        :arguments [],
+                        :state "running",
+                        :tickingRate {:__class__ "UziTickingRateNode",
+                                      :value 1,
+                                      :scale "s"},
+                        :body {:__class__ "UziBlockNode",
+                               :statements []}}]}
+        expected {:__class__ "UziProgram",
+                  :scripts [{:__class__ "UziScript",
+                             :arguments [],
+                             :delay {:__class__ "UziVariable",
+                                     :name nil,
+                                     :value 1000},
+                             :instructions [],
+                             :locals [],
+                             :name "empty",
+                             :ticking true}],
+                  :variables [{:__class__ "UziVariable",
+                               :name nil,
+                               :value 1000}]}
+        actual (compile-node ast)]
+    (is (= expected actual))))
