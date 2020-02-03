3d51f29622acc0cca7446ee95f23006d5e70db03
RichoM <richi.moran@gmail.com>
2020-02-03 14:37:14 -0300

    Added more incomplete (and slightly incorrect) compiler functions. Also, another test.

--- clojure/plugin/src/plugin/compiler.clj
+++ clojure/plugin/src/plugin/compiler.clj
@@ -1,19 +1,25 @@
 (ns plugin.compiler
   (:require [cheshire.core :refer [parse-string]]))
 
-(defmulti compile :__class__)
+(defmulti compile-node :__class__)
 
-(defmethod compile "UziProgramNode" [node]
+(defn compile [node path]
+   (println "node: " (node :__class__))
+   (println "path: "  (map :__class__ path))
+   (println)
+   (compile-node node (conj path node)))
+
+(defmethod compile-node "UziProgramNode" [node path]
   {:__class__ "UziProgram"
    :variables (->> (node :scripts)
-                   (map #(-> % :tickingRate))
-                   (map compile)
+                   (map #(% :tickingRate))
+                   (map #(compile-node % path))
                    vec)
    :scripts (->> (node :scripts)
-                 (map compile)
+                 (map #(compile % path))
                  vec)})
 
-(defmethod compile "UziTickingRateNode" [node]
+(defmethod compile-node "UziTickingRateNode" [node path]
   (let [delay-ms (if (= (node :value) 0)
                    (double Double/MAX_VALUE)
                    (/ (case (node :scale)
@@ -25,17 +31,50 @@
     {:__class__ "UziVariable"
      :value delay-ms}))
 
-(defmethod compile "UziTaskNode" [node]
+(defmethod compile-node "UziTaskNode" [node path]
   {:__class__ "UziScript",
    :arguments [],
-   :delay (compile (node :tickingRate)),
-   :instructions [],
+   :delay (compile (node :tickingRate) path),
+   :instructions (compile (node :body) path),
    :locals [],
    :name (node :name),
    :ticking (contains? #{"running" "once"}
                        (node :state))})
 
-(defmethod compile :default [_] :oops)
+(defmethod compile-node "UziBlockNode" [node path]
+  (vec (flatten (map #(compile % path) (node :statements))))) ; TODO(Richo): Add pop instruction if last stmt is expr
+
+(defmethod compile-node "UziAssignmentNode" [node path]
+  (let [right (compile (node :right) path)
+        var-name (-> node :left :name)]
+    (conj right
+          {:__class__ "UziPopInstruction"
+           :argument {:__class__ "UziVariable"
+                      :name var-name}})))
+
+(defmethod compile-node "UziCallNode" [node path] ; TODO(Richo): Detect primitive calls correctly!
+  (conj (vec (flatten (map #(compile (:value %) path)
+                           (node :arguments))))
+        {:__class__ "UziPrimitiveCallInstruction"
+         :argument {:__class__ "UziPrimitive"
+                    :name "add"}}))
+
+(defmethod compile-node "UziNumberLiteralNode" [node path]
+  [{:__class__ "UziPushInstruction"
+    :argument {:__class__ "UziVariable"
+               :value (node :value)}}])
+
+(defmethod compile-node "UziVariableNode" [node path]
+  [{:__class__ "UziPushInstruction"
+    :argument {:__class__ "UziVariable"
+               :name (node :name)}}])
+
+(defmethod compile-node :default [node _]
+  (println "ERROR! Unknown node: " (:__class__ node))
+  :oops)
+
+(defn compile-tree [ast]
+  (compile ast (list)))
 
 (defn compile-json-string [str]
-  (compile (parse-string str true)))
+  (compile-tree (parse-string str true)))
--- clojure/plugin/test/plugin/compiler_test.clj
+++ clojure/plugin/test/plugin/compiler_test.clj
@@ -25,5 +25,71 @@
                              :ticking true}],
                   :variables [{:__class__ "UziVariable",
                                :value 1000}]}
-        actual (compile ast)]
+        actual (compile-tree ast)]
+    (is (equivalent? expected actual))))
+
+
+(deftest program-with-global-variable-test
+  (let [ast {:__class__ "UziProgramNode",
+             :globals [{:__class__ "UziVariableDeclarationNode",
+                        :name "counter"}],
+             :scripts [{:__class__ "UziTaskNode",
+                        :name "empty",
+                        :arguments [],
+                        :state "running",
+                        :tickingRate {:__class__ "UziTickingRateNode",
+                                      :value 1,
+                                      :scale "s"},
+                        :body {:__class__ "UziBlockNode",
+                               :statements [{:__class__ "UziAssignmentNode",
+                                             :left {:__class__ "UziVariableNode",
+                                                    :name "counter"},
+                                             :right {:__class__ "UziCallNode",
+                                                     :selector "+",
+                                                     :arguments [{:__class__ "Association",
+                                                                  :value {:__class__ "UziVariableNode",
+                                                                          :name "counter"}},
+                                                                 {:__class__ "Association",
+                                                                  :value {:__class__ "UziNumberLiteralNode",
+                                                                          :value 1}}]}}]}}],
+             :primitives []}
+        expected {:__class__ "UziProgram",
+                  :scripts [{:__class__ "UziScript",
+                             :arguments [],
+                             :delay {:__class__ "UziVariable",
+                                     :value 1000},
+                             :instructions [{:__class__ "UziPushInstruction",
+                                             :argument {:__class__ "UziVariable",
+                                                        :name "counter",
+                                                        ;:value 0
+                                                        }},
+                                            {:__class__ "UziPushInstruction",
+                                             :argument {:__class__ "UziVariable",
+                                                        :value 1}},
+                                            {:__class__ "UziPrimitiveCallInstruction",
+                                             :argument {:__class__ "UziPrimitive",
+                                                        ;:code 6,
+                                                        :name "add",
+                                                        ;:stackTransition {:__class__ "Association",
+                                                        ;                  :key 2,
+                                                        ;                  :value 1}
+                                                        }},
+                                            {:__class__ "UziPopInstruction",
+                                             :argument {:__class__ "UziVariable",
+                                                        :name "counter",
+                                                        ;:value 0
+                                                        }}],
+                             :locals [],
+                             :name "empty",
+                             :ticking true}],
+                  :variables [;{:__class__ "UziVariable",
+                              ; :name "counter",
+                              ; :value 0},
+                              {:__class__ "UziVariable",
+                               :value 1000},
+                              ;{:__class__ "UziVariable",
+                              ; :value 1}
+                              ]
+                  }
+        actual (compile-tree ast)]
     (is (equivalent? expected actual))))
