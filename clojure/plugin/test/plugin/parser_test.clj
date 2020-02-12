4d588c6ae6e6445c94f6f105ee4c45a929974707
Matias Teragni <matias.teragni@gmail.com>
2020-02-12 15:25:27 -0300

    Added a test for a procedure with a single argument

--- clojure/plugin/test/plugin/parser_test.clj
+++ clojure/plugin/test/plugin/parser_test.clj
@@ -6,8 +6,13 @@
 
 
 
+(deftest sanity-check
+  (testing "Sanity check."
+    (is (= 1 1))))
+
 (deftest empty-program
- (testing "An Empty Program" (let [src ""
+ (testing "An Empty Program"
+   (let [src ""
         expected {:__class__ "UziProgramNode",
                   :imports [],
                   :globals [],
@@ -17,7 +22,8 @@
     (is (equivalent? expected actual)))))
 
 (deftest blink13
- (testing "A Task that blinks the pin 13" (let [src "task default() running 1/s {\n\ttoggle(D13);\n}"
+ (testing "A Task that blinks the pin 13"
+   (let [src "task default() running 1/s {\n\ttoggle(D13);\n}"
         expected {:__class__ "UziProgramNode",
                   :imports [],
                   :globals [],
@@ -44,9 +50,35 @@
                   :primitives []}
         actual (parse src) ]
     (is (equivalent? expected actual)))))
-
-(deftest sanity-check
-  (testing "Sanity check."
-    (is (= 1 1))))
-
-
+(deftest procedure-with-argument
+  (testing "A procedure with a single argument"
+    (let [src "proc blink(arg0) {\n\tturnOn(arg0);\n\tdelayS(1);\n\tturnOff(arg0);\n}"
+          expected  {:__class__ "UziProgramNode",
+                     :imports [],
+                     :globals [],
+                     :scripts [{:__class__ "UziProcedureNode",
+                                :name "blink",
+                                :arguments [{:__class__ "UziVariableDeclarationNode",
+                                             :name "arg0"}],
+                                :body {:__class__ "UziBlockNode",
+                                       :statements [{:__class__ "UziCallNode",
+                                                     :selector "turnOn",
+                                                     :arguments [{:__class__ "Association",
+                                                                  :key nil,
+                                                                  :value {:__class__ "UziVariableNode",
+                                                                          :name "arg0"}}]},
+                                                    {:__class__ "UziCallNode",
+                                                     :selector "delayS",
+                                                     :arguments [{:__class__ "Association",
+                                                                  :key nil,
+                                                                  :value {:__class__ "UziNumberLiteralNode",
+                                                                          :value 1}}]},
+                                                    {:__class__ "UziCallNode",
+                                                     :selector "turnOff",
+                                                     :arguments [{:__class__ "Association",
+                                                                  :key nil,
+                                                                  :value {:__class__ "UziVariableNode",
+                                                                          :name "arg0"}}]}]}}],
+                     :primitives []}
+          actual (parse src)]
+      (is (equivalent? expected actual)))))
