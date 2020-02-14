d6c27b8ba0766c17221c1ebb75a83e071335ade9
RichoM <richi.moran@gmail.com>
2020-02-14 19:47:45 -0300

    Added ast-utils/node? function

--- clojure/plugin/src/plugin/compiler/ast_utils.clj
+++ clojure/plugin/src/plugin/compiler/ast_utils.clj
@@ -1,6 +1,10 @@
 (ns plugin.compiler.ast-utils
   (:require [clojure.walk :as w]))
 
+(defn node? [node]
+  (and (map? node)
+       (contains? node :__class__)))
+
 (defn filter [ast & types]
   (let [type-set (into #{} types)
         result (atom [])]
