8bb11957659646bb97e29da19fc5dbf9b7d8bbe3
RichoM <richi.moran@gmail.com>
2020-02-14 19:49:46 -0300

    Added tests for the new ast-utils/transform macros

--- clojure/plugin/test/plugin/compiler_test.clj
+++ clojure/plugin/test/plugin/compiler_test.clj
@@ -1,5 +1,6 @@
 (ns plugin.compiler-test
   (:require [clojure.test :refer :all]
+            [plugin.compiler.ast-utils :as ast-utils]
             [plugin.compiler.core :as compiler :refer [compile-tree]])
   (:use [plugin.test-utils]))
 
@@ -141,57 +142,185 @@
 
 ; TODO(RICHO): Uncomment this test and make it pass!
 #_(deftest program-with-local-variable
-    (let [ast {:__class__ "UziProgramNode",
-               :scripts [{:__class__ "UziTaskNode",
-                          :name "foo",
-                          :arguments [],
-                          :state "once",
-                          :tickingRate nil,
-                          :body {:__class__ "UziBlockNode",
-                                 :statements [{:__class__ "UziVariableDeclarationNode",
-                                               :name "pin",
-                                               :value {:__class__ "UziPinLiteralNode",
-                                                       :type "D",
-                                                       :number 13}},
-                                              {:__class__ "UziCallNode",
-                                               :selector "toggle",
-                                               :arguments [{:__class__ "Association",
-                                                            :key nil,
-                                                            :value {:__class__ "UziVariableNode",
-                                                                    :name "pin"}}]}]}}]}
-          expected {:__class__ "UziProgram",
-                    :scripts [{:__class__ "UziScript",
-                               :name "foo",
-                               :ticking true,
-                               :delay {:__class__ "UziVariable",
-                                       :value 0},
-                               :locals [{:__class__ "UziVariable",
-                                         :name "pin#1",
-                                         :value 0}],
-                               :instructions [{:__class__ "UziPushInstruction",
-                                               :argument {:__class__ "UziVariable",
-                                                          :value 13}},
-                                              {:__class__ "UziWriteLocalInstruction",
-                                               :argument {:__class__ "UziVariable",
-                                                          :name "pin#1",
-                                                          :value 0}},
-                                              {:__class__ "UziReadLocalInstruction",
-                                               :argument {:__class__ "UziVariable",
-                                                          :name "pin#1",
-                                                          :value 0}},
-                                              {:__class__ "UziPrimitiveCallInstruction",
-                                               :argument {:__class__ "UziPrimitive",
-                                                          ;:code 2,
-                                                          :name "toggle",
-                                                          ;:stackTransition {:__class__ "Association",
-                                                          ;                  :key 1,
-                                                          ;                  :value 0}
-                                                          }},
-                                              {:__class__ "UziStopScriptInstruction",
-                                               :argument "foo"}]}],
-                    :variables #{{:__class__ "UziVariable",
-                                  :value 0},
-                                 {:__class__ "UziVariable",
-                                  :value 13}}}
-          actual (compile-tree ast)]
-      (is (equivalent? expected actual))))
+  (let [ast {:__class__ "UziProgramNode",
+             :scripts [{:__class__ "UziTaskNode",
+                        :name "foo",
+                        :arguments [],
+                        :state "once",
+                        :tickingRate nil,
+                        :body {:__class__ "UziBlockNode",
+                               :statements [{:__class__ "UziVariableDeclarationNode",
+                                             :name "pin",
+                                             :value {:__class__ "UziPinLiteralNode",
+                                                     :type "D",
+                                                     :number 13}},
+                                            {:__class__ "UziCallNode",
+                                             :selector "toggle",
+                                             :arguments [{:__class__ "Association",
+                                                          :key nil,
+                                                          :value {:__class__ "UziVariableNode",
+                                                                  :name "pin"}}]}]}}]}
+        expected {:__class__ "UziProgram",
+                  :scripts [{:__class__ "UziScript",
+                             :name "foo",
+                             :ticking true,
+                             :delay {:__class__ "UziVariable",
+                                     :value 0},
+                             :locals [{:__class__ "UziVariable",
+                                       :name "pin#1",
+                                       :value 0}],
+                             :instructions [{:__class__ "UziPushInstruction",
+                                             :argument {:__class__ "UziVariable",
+                                                        :value 13}},
+                                            {:__class__ "UziWriteLocalInstruction",
+                                             :argument {:__class__ "UziVariable",
+                                                        :name "pin#1",
+                                                        :value 0}},
+                                            {:__class__ "UziReadLocalInstruction",
+                                             :argument {:__class__ "UziVariable",
+                                                        :name "pin#1",
+                                                        :value 0}},
+                                            {:__class__ "UziPrimitiveCallInstruction",
+                                             :argument {:__class__ "UziPrimitive",
+                                                        ;:code 2,
+                                                        :name "toggle",
+                                                        ;:stackTransition {:__class__ "Association",
+                                                        ;                  :key 1,
+                                                        ;                  :value 0}
+                                                        }},
+                                            {:__class__ "UziStopScriptInstruction",
+                                             :argument "foo"}]}],
+                  :variables #{{:__class__ "UziVariable",
+                                :value 0},
+                               {:__class__ "UziVariable",
+                                :value 13}}}
+        actual (compile-tree ast)]
+    (is (equivalent? expected actual))))
+
+
+(deftest ast-transform-test
+  (let [original {:__class__ "UziProgramNode",
+                  :scripts [{:__class__ "UziTaskNode",
+                             :name "empty",
+                             :arguments [],
+                             :state "running",
+                             :tickingRate {:__class__ "UziTickingRateNode",
+                                           :value 1,
+                                           :scale "s"},
+                             :body {:__class__ "UziBlockNode",
+                                    :statements [{:__class__ "UziCallNode",
+                                                  :selector "toggle",
+                                                  :arguments [{:__class__ "Association",
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :value 13}}]},
+                                                 {:__class__ "UziCallNode",
+                                                  :selector "turnOn",
+                                                  :arguments [{:__class__ "Association",
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :value 12}}]}]}}]}
+        expected {:__class__ "UziProgramNode",
+                  :__foo__ 1,
+                  :scripts [{:__class__ "UziTaskNode",
+                             :__foo__ 2,
+                             :name "empty",
+                             :arguments [],
+                             :state "running",
+                             :tickingRate {:__class__ "UziTickingRateNode",
+                                           :__bar__ 5,
+                                           :value 1,
+                                           :scale "s"},
+                             :body {:__class__ "UziBlockNode",
+                                    :__bar__ 5,
+                                    :statements [{:__class__ "UziCallNode",
+                                                  :__foo__ 3,
+                                                  :selector "toggle",
+                                                  :arguments [{:__class__ "Association",
+                                                               :__bar__ 5,
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :__foo__ 4
+                                                                       :value 13}}]},
+                                                 {:__class__ "UziCallNode",
+                                                  :__foo__ 3,
+                                                  :selector "turnOn",
+                                                  :arguments [{:__class__ "Association",
+                                                               :__bar__ 5,
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :__foo__ 4,
+                                                                       :value 12}}]}]}}]}
+        actual (ast-utils/transform
+                original
+                "UziProgramNode" #(assoc % :__foo__ 1)
+                "UziTaskNode" #(assoc % :__foo__ 2)
+                "UziCallNode" #(assoc % :__foo__ 3)
+                "UziNumberLiteralNode" #(assoc % :__foo__ 4)
+                :default #(assoc % :__bar__ 5))]
+    (is (= expected actual))))
+
+
+
+
+(deftest ast-transform-pred-test
+  (let [original {:__class__ "UziProgramNode",
+                  :scripts [{:__class__ "UziTaskNode",
+                             :name "empty",
+                             :arguments [],
+                             :state "running",
+                             :tickingRate {:__class__ "UziTickingRateNode",
+                                           :value 1,
+                                           :scale "s"},
+                             :body {:__class__ "UziBlockNode",
+                                    :statements [{:__class__ "UziCallNode",
+                                                  :selector "toggle",
+                                                  :arguments [{:__class__ "Association",
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :value 13}}]},
+                                                 {:__class__ "UziCallNode",
+                                                  :selector "turnOn",
+                                                  :arguments [{:__class__ "Association",
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :value 12}}]}]}}]}
+        expected {:__class__ "UziProgramNode",
+                  :__foo__ 1,
+                  :scripts [{:__class__ "UziTaskNode",
+                             :__foo__ 2,
+                             :name "empty",
+                             :arguments [],
+                             :state "running",
+                             :tickingRate {:__class__ "UziTickingRateNode",
+                                           :__bar__ 5,
+                                           :value 1,
+                                           :scale "s"},
+                             :body {:__class__ "UziBlockNode",
+                                    :__bar__ 5,
+                                    :statements [{:__class__ "UziCallNode",
+                                                  :__foo__ 3,
+                                                  :selector "toggle",
+                                                  :arguments [{:__class__ "Association",
+                                                               :__bar__ 5,
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :__foo__ 4
+                                                                       :value 13}}]},
+                                                 {:__class__ "UziCallNode",
+                                                  :__foo__ 3,
+                                                  :selector "turnOn",
+                                                  :arguments [{:__class__ "Association",
+                                                               :__bar__ 5,
+                                                               :key nil,
+                                                               :value {:__class__ "UziNumberLiteralNode",
+                                                                       :__foo__ 4,
+                                                                       :value 12}}]}]}}]}
+        actual (ast-utils/transform-pred
+                original
+                #(= "UziProgramNode" (get % :__class__)) #(assoc % :__foo__ 1)
+                #(= "UziTaskNode" (get % :__class__)) #(assoc % :__foo__ 2)
+                #(= "UziCallNode" (get % :__class__)) #(assoc % :__foo__ 3)
+                #(= "UziNumberLiteralNode" (get % :__class__)) #(assoc % :__foo__ 4)
+                :default #(assoc % :__bar__ 5))]
+    (is (= expected actual))))
