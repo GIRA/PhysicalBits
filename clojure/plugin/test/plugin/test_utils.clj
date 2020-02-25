8a66e36fe0ddfead40ffd55260ea3f7a43e1c61a
RichoM <richi.moran@gmail.com>
2020-02-25 09:17:17 -0300

    Added TODO comment

--- clojure/plugin/test/plugin/test_utils.clj
+++ clojure/plugin/test/plugin/test_utils.clj
@@ -22,5 +22,6 @@
                      :diffs (ultra-test/generate-diffs expected# [actual#])}))
        result#)))
 
+; TODO(Richo): Change implementation for sets and vectors so that it checks equality
 (defn equivalent? [a b]
   (-> (data/diff a b) first nil?))
