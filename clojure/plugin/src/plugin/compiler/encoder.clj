7a8b2d90c2b15ffba3222c763db4c9a8e9312d82
Ricardo Moran <richi.moran@gmail.com>
2020-04-13 23:20:32 -0300

    Implemented encoding of global variables and script header.
    Added a few tests for the encoder

--- clojure/plugin/src/plugin/compiler/encoder.clj
+++ clojure/plugin/src/plugin/compiler/encoder.clj
@@ -1,48 +1,102 @@
-(ns plugin.compiler.encoder)
+(ns plugin.compiler.encoder
+  (:require [plugin.utils.conversions :refer :all]))
 
-(defmulti encode-node :__class__)
+(defmulti encode-instruction :__class__)
 
-(defn encode [program]
-  (vec (encode-node program)))
+(defn variable-size
+  "Return the number of bytes necessary to encode this variable.
+	If the value is negative or float then the size is 4 bytes. Also, the
+	max number of bytes is 4."
+  [value]
+  (if (or (float? value) (neg? value))
+    4
+    (or (first (filter (fn [size] (< value (Math/pow 2 (* 8 size))))
+                       [1 2 3 4]))
+        4)))
 
-(def default-globals #{0 1 -1})
+(defn sort-globals [program]
+  (let [sorted-globals (sort-by (fn [{:keys [value]}] [value (variable-size value)])
+                                (fn [[a-value a-size] [b-value b-size]]
+                                  (if (= a-size b-size)
+                                    (< a-value b-value)
+                                    (< a-size b-size)))
+                                (:globals program))]
+    (assoc program :sorted-globals (vec sorted-globals))))
+
+(def default-globals [0 1 -1])
+
+(defn all-globals [{globals :sorted-globals}]
+  (concat default-globals
+          (filter (complement (set default-globals))
+                  (map :value globals))))
 
 (defn encode-globals [globals]
-  (let [all-globals (into (map :value globals)
-                          default-globals)
-        to-encode (filter (complement default-globals)
-                          all-globals)]
-    (concat [(count to-encode)])))
+  (let [to-encode (filter (complement (set default-globals))
+                          (map :value globals))
+        groups (group-by variable-size to-encode)
+        encode-global (fn [value size]
+                        (let [actual-value (if (= 4 size)
+                                             (float->uint32 value)
+                                             value)]
+                          (map (fn [n]
+                                 (bit-and (bit-shift-right actual-value
+                                                           (* 8 n))
+                                          16rFF))
+                               (range (dec size) -1 -1))))
+        encode-group-size (fn [size group]
+                            (bit-or (bit-shift-left (count group) 2)
+                                    (dec size)))]
+    (concat [(count to-encode)]
+            (mapcat (fn [size]
+                      (mapcat (fn [partition]
+                                (concat [(encode-group-size size partition)]
+                                        (mapcat #(encode-global % size)
+                                                partition)))
+                              (partition-all 2r111111 (groups size))))
+                    [1 2 3 4]))))
 
-(defmethod encode-node "UziProgram"
-  [{:keys [scripts variables] :as program}]
-  (concat [(count scripts)]
-          (encode-globals variables)
-          (mapcat encode-node scripts)))
 
 (defn encode-script-header
-  [{:keys [arguments delay locals ticking]}]
-  (concat [0]))
+  [{:keys [arguments delay locals running?]} globals]
+  (let [has-delay? (> (:value delay) 0)
+        has-arguments? (not (empty? arguments))
+        has-locals? (not (empty? locals))]
+    (concat [(bit-and (bit-or (bit-shift-left (if running? 1 0) 7)
+                              (bit-shift-left (if has-delay? 1 0) 6)
+                              (bit-shift-left (if has-arguments? 1 0) 5)
+                              (bit-shift-left (if has-locals? 1 0) 4))
+                      16rFF)]
+            (if has-delay?
+              [(.indexOf globals (:value delay))]
+              []))))
 
 (defn encode-instructions [instructions]
   (concat [(count instructions)]
-          (mapcat encode-node instructions)))
+          (mapcat encode-instruction instructions)))
 
-(defmethod encode-node "UziScript"
-  [{:keys [instructions] :as script}]
-  (concat (encode-script-header script)
+(defn encode-script [{:keys [instructions] :as script} globals]
+  (concat (encode-script-header script globals)
           (encode-instructions instructions)))
 
-(defmethod encode-node :default [o] [])
+(defn encode-program [{:keys [scripts sorted-globals] :as program}]
+  (concat [(count scripts)]
+          (encode-globals sorted-globals)
+          (mapcat (fn [script]
+                    (encode-script script (all-globals program)))
+                  scripts)))
+
+(defmethod encode-instruction :default [o] [])
+
+(defn encode [program]
+  (-> program
+      sort-globals
+      encode-program
+      vec))
 
 #_(
-   (def src "")
-   (def program (plugin.compiler.core/compile-uzi-string src))
-
-    (encode program)
-   (encode-script-count (:scripts program))
-   (encode-globals (:globals program))
-   (mapcat encode-node (:scripts program))
+   (def src "task main() running 1/s {}")
+   (def program (sort-globals (plugin.compiler.core/compile-uzi-string src)))
+   program
 
 
 
@@ -50,9 +104,10 @@
 
 
 
-
-
-
+   (all-globals program)
+   (encode-globals (:sorted-globals program))
+   (encode-script-header (get (:scripts program) 0) (all-globals program))
+   (encode-program program)
 
 
 
--- clojure/plugin/test/plugin/encoder_test.clj
+++ clojure/plugin/test/plugin/encoder_test.clj
@@ -15,3 +15,21 @@
   (let [expected [0 0]
         actual (encode "")]
     (is (= expected actual))))
+
+(deftest empty-script
+  (let [expected [1 1 5 3 232 192 3 0]
+        actual (encode "task main() running 1/s {}")]
+    (is (= expected actual))))
+
+(deftest variables-have-size
+  (doseq [[value size] {0 1,
+                        16rFF 1,
+                        16r100 2,
+                        16rFFFF 2,
+                        16r10000 3,
+                        16rFFFFFF 3,
+                        16r1000000 4,
+                        16rFFFFFFFFFF 4,
+                        0.5 4
+                        -1 4}]
+    (is (= size (en/variable-size value)))))
