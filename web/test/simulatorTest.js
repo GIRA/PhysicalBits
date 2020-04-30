a2b498d7e7835671cf6e23f801c7b46bf64e1ebe
Ricardo Moran <richi.moran@gmail.com>
2020-04-30 16:43:02 -0300

    Removed commented code

--- web/test/simulatorTest.js
+++ web/test/simulatorTest.js
@@ -21,7 +21,7 @@ describe('Simulator Tests', function () {
     sim.stop();
     sim = null;
   });
-  
+
   it('set/get pin value', function () {
     sim.setPinValue(6, 1);
     assert.equal(1, sim.getPinValue(6));
@@ -36,4 +36,3 @@ describe('Simulator Tests', function () {
     assert.equal(0, sim.getPinValue(6));
   });
 });
-//   sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":0},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":6}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":21,"name":"turnOff","stackTransition":{"__class__":"Association","key":1,"value":0}}},{"__class__":"UziStopScriptInstruction","argument":"test"}],"locals":[],"name":"test","ticking":true}],"variables":[{"__class__":"UziVariable","name":null,"value":0},{"__class__":"UziVariable","name":null,"value":6}]});
