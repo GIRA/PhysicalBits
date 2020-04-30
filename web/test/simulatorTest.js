efa97bf5c034505a6d2e13c0bfc35f69a0291a49
Ricardo Moran <richi.moran@gmail.com>
2020-04-30 16:26:54 -0300

    Small refactoring of tests

--- web/test/simulatorTest.js
+++ web/test/simulatorTest.js
@@ -10,19 +10,25 @@ it('sanity-check', function () {
   assert.equal(0, 0);
 });
 
-it('set/get pin value', function () {
-  let sim = initializeSimulator();
-  sim.setPinValue(6, 1);
-  assert.equal(1, sim.getPinValue(6));
-});
+describe('Simulator Tests', function () {
+  let sim = null;
 
-it('turn off pin', function() {
-  let sim = initializeSimulator();
-  sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":0},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":6}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":21,"name":"turnOff","stackTransition":{"__class__":"Association","key":1,"value":0}}},{"__class__":"UziStopScriptInstruction","argument":"test"}],"locals":[],"name":"test","ticking":true}],"variables":[{"__class__":"UziVariable","name":null,"value":0},{"__class__":"UziVariable","name":null,"value":6}]});
-  sim.setPinValue(6, 1);
-  for (let i = 0; i < 2; i++) {
-    sim.execute();
-  }
-  assert.equal(0, sim.getPinValue(6));
+  beforeEach(function () {
+    sim = initializeSimulator();
+  });
+
+  it('set/get pin value', function () {
+    sim.setPinValue(6, 1);
+    assert.equal(1, sim.getPinValue(6));
+  });
+
+  it('turn off pin', function() {
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":0},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":6}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":21,"name":"turnOff","stackTransition":{"__class__":"Association","key":1,"value":0}}},{"__class__":"UziStopScriptInstruction","argument":"test"}],"locals":[],"name":"test","ticking":true}],"variables":[{"__class__":"UziVariable","name":null,"value":0},{"__class__":"UziVariable","name":null,"value":6}]});
+    sim.setPinValue(6, 1);
+    for (let i = 0; i < 2; i++) {
+      sim.execute();
+    }
+    assert.equal(0, sim.getPinValue(6));
+  });
 });
 //   sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":0},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":6}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":21,"name":"turnOff","stackTransition":{"__class__":"Association","key":1,"value":0}}},{"__class__":"UziStopScriptInstruction","argument":"test"}],"locals":[],"name":"test","ticking":true}],"variables":[{"__class__":"UziVariable","name":null,"value":0},{"__class__":"UziVariable","name":null,"value":6}]});
