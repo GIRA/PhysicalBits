93143ac8885043def01a3f4ded257f1c02b10ef1
Ricardo Moran <richi.moran@gmail.com>
2020-05-01 15:57:37 -0300

    Added a few more validations to the checker

--- clojure/middleware/src/middleware/compiler/checker.clj
+++ clojure/middleware/src/middleware/compiler/checker.clj
@@ -10,6 +10,11 @@
     (register-error! description node errors))
   bool)
 
+(defn assert-statement [node errors]
+  (assert (ast-utils/statement? node)
+          "Statement expected"
+          node errors))
+
 (defmulti check-node (fn [node errors path] (:__class__ node)))
 
 (defn- check [node errors path]
@@ -20,7 +25,36 @@
 (defmethod check-node "UziProgramNode" [node errors path]
   (let [imports (:imports node)]
     (doseq [import imports]
+      ; TODO(Richo): ?
       )))
+
+(defmethod check-node "UziTaskNode" [node errors path]
+  )
+
+(defmethod check-node "UziCallNode" [node errors path])
+
+(defmethod check-node "UziBlockNode" [node errors path]
+  (doseq [stmt (:statements node)]
+    (assert-statement stmt errors)))
+
+
+(defmethod check-node "UziNumberLiteralNode" [node errors path]
+  (assert (number? (:value node))
+          "Number expected"
+          node
+          errors))
+
+(defmethod check-node "UziPinLiteralNode" [node errors path]
+  (assert (contains? #{"D" "A"} (:type node))
+          "Invalid pin type"
+          node
+          errors)
+  (assert (pos-int? (:number node))
+          "Positive integer expected"
+          node
+          errors))
+
+(defmethod check-node "Association" [_ _ _]) ; TODO(Richo): Remove
 
 (defmethod check-node :default [node _ _]
   (throw (ex-info "MISSING: " node)))
@@ -30,3 +64,13 @@
         path (list)]
     (check ast errors path)
     @errors))
+
+#_(
+   (do ; Definitions
+     (def parse middleware.parser.parser/parse)
+     (def pprint clojure.pprint/pprint)
+     (def ast (parse "task foo() stopped {F4;}")))
+  (pprint ast)
+  (check-tree ast)
+
+  )
