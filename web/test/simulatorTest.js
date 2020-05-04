335f273f08828d28ddf3bd383b5bc8f6b2c96186
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2020-05-04 15:39:40 -0300

    Changed parameters in sim.loadProgram() to make it more compact and the script is not that long

--- web/test/simulatorTest.js
+++ web/test/simulatorTest.js
@@ -37,58 +37,7 @@ describe('Simulator Tests', function () {
   });
 
   it('turn on pin', function() {
-    sim.loadProgram({
-      "__class__": "UziProgram",
-      "scripts": [
-       {
-        "__class__": "UziScript",
-        "arguments": [],
-        "delay": {
-         "__class__": "UziVariable",
-         "name": null,
-         "value": 1000
-        },
-        "instructions": [
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 6
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 20,
-           "name": "turnOn",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 1,
-            "value": 0
-           }
-          }
-         }
-        ],
-        "locals": [],
-        "name": "default",
-        "ticking": true
-       }
-      ],
-      "variables": [
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 1000
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 6
-       }
-      ]
-     })
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":1000},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":6}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":20,"name":"turnOn","stackTransition":{"__class__":"Association","key":1,"value":0}}}],"locals":[],"name":"default","ticking":true,"nextRun":336503,"lastStart":406}],"variables":[{"__class__":"UziVariable","name":null,"value":1000},{"__class__":"UziVariable","name":null,"value":6}]})
      sim.setPinValue(6,0);
      for (let i = 0; i < 2; i++) {
       sim.execute();
@@ -97,58 +46,7 @@ describe('Simulator Tests', function () {
   });
 
   it('toggle pin', function() {
-    sim.loadProgram({
-      "__class__": "UziProgram",
-      "scripts": [
-       {
-        "__class__": "UziScript",
-        "arguments": [],
-        "delay": {
-         "__class__": "UziVariable",
-         "name": null,
-         "value": 1000
-        },
-        "instructions": [
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 6
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 2,
-           "name": "toggle",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 1,
-            "value": 0
-           }
-          }
-         }
-        ],
-        "locals": [],
-        "name": "default",
-        "ticking": true
-       }
-      ],
-      "variables": [
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 1000
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 6
-       }
-      ]
-     });
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":1000},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":6}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":2,"name":"toggle","stackTransition":{"__class__":"Association","key":1,"value":0}}}],"locals":[],"name":"default","ticking":true,"nextRun":61384,"lastStart":12}],"variables":[{"__class__":"UziVariable","name":null,"value":1000},{"__class__":"UziVariable","name":null,"value":6}]});
      sim.setPinValue(6,0);
      for(let i = 0; i < 2; i++)
      {
@@ -163,84 +61,7 @@ describe('Simulator Tests', function () {
   });
 
   it('multiplication', function(){ // 2 * 3
-    sim.loadProgram({
-      "__class__": "UziProgram",
-      "scripts": [
-       {
-        "__class__": "UziScript",
-        "arguments": [],
-        "delay": {
-         "__class__": "UziVariable",
-         "name": null,
-         "value": 1000
-        },
-        "instructions": [
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 2
-          }
-         },
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 3
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 5,
-           "name": "multiply",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 2,
-            "value": 1
-           }
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 20,
-           "name": "turnOn",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 1,
-            "value": 0
-           }
-          }
-         }
-        ],
-        "locals": [],
-        "name": "default",
-        "ticking": true
-       }
-      ],
-      "variables": [
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 1000
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 2
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 3
-       }
-      ]
-     });
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":1000},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":2}},{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":3}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":5,"name":"multiply","stackTransition":{"__class__":"Association","key":2,"value":1}}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":20,"name":"turnOn","stackTransition":{"__class__":"Association","key":1,"value":0}}}],"locals":[],"name":"default","ticking":true,"nextRun":10744,"lastStart":18}],"variables":[{"__class__":"UziVariable","name":null,"value":1000},{"__class__":"UziVariable","name":null,"value":2},{"__class__":"UziVariable","name":null,"value":3}]});
      for(let i = 0; i < 3; i++){
        sim.execute();
      }
@@ -248,173 +69,15 @@ describe('Simulator Tests', function () {
   });
 
   it('division', function(){ /* 12 / 4 */
-    sim.loadProgram({
-      "__class__": "UziProgram",
-      "scripts": [
-       {
-        "__class__": "UziScript",
-        "arguments": [],
-        "delay": {
-         "__class__": "UziVariable",
-         "name": null,
-         "value": 1000
-        },
-        "instructions": [
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 12
-          }
-         },
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 3
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 7,
-           "name": "divide",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 2,
-            "value": 1
-           }
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 20,
-           "name": "turnOn",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 1,
-            "value": 0
-           }
-          }
-         }
-        ],
-        "locals": [],
-        "name": "default",
-        "ticking": true,
-        "nextRun": 540224,
-        "lastStart": 493
-       }
-      ],
-      "variables": [
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 1000
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 12
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 3
-       }
-      ]
-     });
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":1000},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":12}},{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":4}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":7,"name":"divide","stackTransition":{"__class__":"Association","key":2,"value":1}}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":20,"name":"turnOn","stackTransition":{"__class__":"Association","key":1,"value":0}}}],"locals":[],"name":"default","ticking":true}],"variables":[{"__class__":"UziVariable","name":null,"value":1000},{"__class__":"UziVariable","name":null,"value":12},{"__class__":"UziVariable","name":null,"value":4}]});
      for(let i = 0; i < 3; i++){
       sim.execute();
     }
-    assert.equal(4, sim.stack[0]);
+    assert.equal(3, sim.stack[0]);
   });
 
   it('addition', function(){ // 3 + 4
-    sim.loadProgram({
-      "__class__": "UziProgram",
-      "scripts": [
-       {
-        "__class__": "UziScript",
-        "arguments": [],
-        "delay": {
-         "__class__": "UziVariable",
-         "name": null,
-         "value": 1000
-        },
-        "instructions": [
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 3
-          }
-         },
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 4
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 6,
-           "name": "add",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 2,
-            "value": 1
-           }
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 20,
-           "name": "turnOn",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 1,
-            "value": 0
-           }
-          }
-         }
-        ],
-        "locals": [],
-        "name": "default",
-        "ticking": true,
-        "nextRun": 152608,
-        "lastStart": 18
-       }
-      ],
-      "variables": [
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 1000
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 3
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 4
-       }
-      ]
-     });
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":1000},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":3}},{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":4}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":6,"name":"add","stackTransition":{"__class__":"Association","key":2,"value":1}}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":20,"name":"turnOn","stackTransition":{"__class__":"Association","key":1,"value":0}}}],"locals":[],"name":"default","ticking":true,"nextRun":58155,"lastStart":19}],"variables":[{"__class__":"UziVariable","name":null,"value":1000},{"__class__":"UziVariable","name":null,"value":3},{"__class__":"UziVariable","name":null,"value":4}]});
      for(let i = 0; i < 3; i++){
       sim.execute();
     }
@@ -422,84 +85,7 @@ describe('Simulator Tests', function () {
   });
 
   it('subtraction', function(){ // 8 - 4
-    sim.loadProgram({
-      "__class__": "UziProgram",
-      "scripts": [
-       {
-        "__class__": "UziScript",
-        "arguments": [],
-        "delay": {
-         "__class__": "UziVariable",
-         "name": null,
-         "value": 1000
-        },
-        "instructions": [
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 8
-          }
-         },
-         {
-          "__class__": "UziPushInstruction",
-          "argument": {
-           "__class__": "UziVariable",
-           "name": null,
-           "value": 4
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 8,
-           "name": "subtract",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 2,
-            "value": 1
-           }
-          }
-         },
-         {
-          "__class__": "UziPrimitiveCallInstruction",
-          "argument": {
-           "__class__": "UziPrimitive",
-           "code": 20,
-           "name": "turnOn",
-           "stackTransition": {
-            "__class__": "Association",
-            "key": 1,
-            "value": 0
-           }
-          }
-         }
-        ],
-        "locals": [],
-        "name": "default",
-        "ticking": true
-       }
-      ],
-      "variables": [
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 1000
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 8
-       },
-       {
-        "__class__": "UziVariable",
-        "name": null,
-        "value": 4
-       }
-      ]
-     });
+    sim.loadProgram({"__class__":"UziProgram","scripts":[{"__class__":"UziScript","arguments":[],"delay":{"__class__":"UziVariable","name":null,"value":1000},"instructions":[{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":8}},{"__class__":"UziPushInstruction","argument":{"__class__":"UziVariable","name":null,"value":4}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":8,"name":"subtract","stackTransition":{"__class__":"Association","key":2,"value":1}}},{"__class__":"UziPrimitiveCallInstruction","argument":{"__class__":"UziPrimitive","code":20,"name":"turnOn","stackTransition":{"__class__":"Association","key":1,"value":0}}}],"locals":[],"name":"default","ticking":true}],"variables":[{"__class__":"UziVariable","name":null,"value":1000},{"__class__":"UziVariable","name":null,"value":8},{"__class__":"UziVariable","name":null,"value":4}]});
     for(let i = 0; i < 3; i++){
       sim.execute();
     }
