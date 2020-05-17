44720333218404fb7569d57c1546ba5cfd5e6aed
Ricardo Moran <richi.moran@gmail.com>
2020-05-17 18:00:43 -0300

    Changed main function to start the device controller event loop

--- clojure/middleware/src/middleware/core.clj
+++ clojure/middleware/src/middleware/core.clj
@@ -1,9 +1,11 @@
 (ns middleware.core
-  (:require [middleware.server.server :as server])
+  (:require [middleware.server.server :as server]
+            [middleware.device.controller :as dc])
   (:gen-class))
 
 (defn -main [& args]
   (time (do
           (println "Starting server...")
+          (dc/start-event-loop)
           (server/start)
           (println "Server started."))))
