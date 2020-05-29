4e2dfd7140c56c8e9faf8dad86314ba13fe5b71f
Matias Teragni <matias.teragni@gmail.com>
2020-05-29 15:28:19 -0300

    Separated printing the block from printing the task

--- clojure/middleware/src/middleware/code_generator/code_generator.clj
+++ clojure/middleware/src/middleware/code_generator/code_generator.clj
@@ -13,10 +13,13 @@
 (defmethod print-node "UziVariableDeclarationNode" [node] (format "var %s = %s;", (:name node) (print-node (:value node))))
 (defmethod print-node "UziNumberLiteralNode" [node] (str (:value node)))
 (defmethod print-node "UziPinLiteralNode" [node] (str (:type node) (:number node)))
-(defmethod print-node "UziTaskNode" [node] (format "task %s()%s%s\n{\n}"
+(defmethod print-node "UziTaskNode" [node] (format "task %s()%s%s\n%s"
                                                    (:name node)
                                                    (if (= "once" (:state node)) "" (str " " (:state node)))
-                                                   (if (nil? (:tickingRate node)) "" (print-node (:tickingRate node)))))
+                                                   (if (nil? (:tickingRate node)) "" (print-node (:tickingRate node)))
+                                                   (print-node (:body node))))
 
 (defmethod print-node "UziTickingRateNode" [node] (format " %d/%s" (:value node) (:scale node)))
+(defmethod print-node "UziBlockNode" [node] (format "{%s\n}" ""))
+
 (defmethod print-node :default [arg] (throw (Exception. (str "Not Implemented node reached: " (:__class__ arg)) )))
