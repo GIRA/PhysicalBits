059285aad892e50bf8c7f1f2434a5103ce0143cc
Matias Teragni <matias.teragni@gmail.com>
2020-05-29 10:53:17 -0300

    Added printing for variable declaration and literal number

--- clojure/middleware/src/middleware/code_generator/code_generator.clj
+++ clojure/middleware/src/middleware/code_generator/code_generator.clj
@@ -5,7 +5,9 @@
 
 (defn print [node] (print-node node))
 
-(defmethod print-node "UziProgramNode" [node] "")
+(defmethod print-node "UziProgramNode" [node] (clojure.string/join "\n" (map print-node (:globals node))))
 
+(defmethod print-node "UziVariableDeclarationNode" [node] (str "var " (:name node) " = " (print-node (:value node))  ";"))
+(defmethod print-node "UziNumberLiteralNode" [node] (str (:value node)))
 
 (defmethod print-node :default [arg] (throw (Exception. (str "Not Implemented node reached: " (:__class__ arg)) )))
