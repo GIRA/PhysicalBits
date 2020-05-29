84fd8c676724a5a7ffb862ec54335177179044bc
Matias Teragni <matias.teragni@gmail.com>
2020-05-29 15:25:10 -0300

    Added the ticking rate to the Task

--- clojure/middleware/src/middleware/code_generator/code_generator.clj
+++ clojure/middleware/src/middleware/code_generator/code_generator.clj
@@ -13,7 +13,10 @@
 (defmethod print-node "UziVariableDeclarationNode" [node] (format "var %s = %s;", (:name node) (print-node (:value node))))
 (defmethod print-node "UziNumberLiteralNode" [node] (str (:value node)))
 (defmethod print-node "UziPinLiteralNode" [node] (str (:type node) (:number node)))
-(defmethod print-node "UziTaskNode" [node] (format "task %s()%s\n{\n}"
+(defmethod print-node "UziTaskNode" [node] (format "task %s()%s%s\n{\n}"
                                                    (:name node)
-                                                   (if (= "once" (:state node)) "" (str " " (:state node)))))
+                                                   (if (= "once" (:state node)) "" (str " " (:state node)))
+                                                   (if (nil? (:tickingRate node)) "" (print-node (:tickingRate node)))))
+
+(defmethod print-node "UziTickingRateNode" [node] (format " %d/%s" (:value node) (:scale node)))
 (defmethod print-node :default [arg] (throw (Exception. (str "Not Implemented node reached: " (:__class__ arg)) )))
