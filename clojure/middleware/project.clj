68c4e0e6001b541e2f58cdf2af2ec690f26e0620
Ricardo Moran <richi.moran@gmail.com>
2020-06-08 14:04:06 -0300

    Updated project name, description, and license. And removed unnecessary files.

--- clojure/middleware/project.clj
+++ clojure/middleware/project.clj
@@ -1,8 +1,8 @@
-(defproject middleware "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject middleware "0.3.0-SNAPSHOT"
+  :description "UziScript middleware"
+  :url "https://github.com/GIRA/UziScript"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.async "0.6.532"]
                  [org.clojure/tools.logging "1.1.0"]
