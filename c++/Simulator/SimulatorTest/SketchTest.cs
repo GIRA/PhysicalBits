fa268bb6d86e91177c7373ccad8e2077fb44bf41
RichoM <richi.moran@gmail.com>
2020-07-02 10:02:28 -0300

    Fixed VM tests

--- c++/Simulator/SimulatorTest/SketchTest.cs
+++ c++/Simulator/SimulatorTest/SketchTest.cs
@@ -18,7 +18,7 @@ namespace SimulatorTest
 
         private const byte RQ_CONNECTION_REQUEST = 255;
         private const byte MAJOR_VERSION = 0;
-        private const byte MINOR_VERSION = 6;
+        private const byte MINOR_VERSION = 7;
         private const byte KEEP_ALIVE = 7;
 
         public TestContext TestContext { get; set; }
