ba3af6542989f835e339c35216c5fd77cfd99ae7
RichoM <richi.moran@gmail.com>
2020-07-03 15:43:55 -0300

    Removed the code to avoid closing the app on macOS.

--- middleware/desktop/main.js
+++ middleware/desktop/main.js
@@ -40,20 +40,7 @@ function createWindow () {
 
 app.whenReady().then(createWindow);
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    server.kill();
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
+  server.kill();
+  app.quit();
 });
