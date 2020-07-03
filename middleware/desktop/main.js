aa457e094d08a595c639b89f07b6cf145397fd4e
RichoM <richi.moran@gmail.com>
2020-07-03 14:09:09 -0300

    Changed electron main to start/stop the uzi server.
    For now, all the paths are hardcoded to the development files. And it needs the uberjar to be created in order to work.

--- middleware/desktop/main.js
+++ middleware/desktop/main.js
@@ -1,10 +1,10 @@
 const { app, BrowserWindow } = require('electron')
 const { spawn } = require('child_process');
 
-/*
-const bat = spawn('java', ['-jar', 'middleware\\middleware-0.3.0-SNAPSHOT-standalone.jar',
-                           '-o', '-w', 'middleware\\web', '-u', 'middleware\\uzi']);
-*/
+// TODO(Richo): Allow to configure paths
+const server = spawn('java', ['-jar', '../server/target/uberjar/middleware-0.3.0-SNAPSHOT-standalone.jar',
+                              '-w', '../../gui',
+                              '-u', '../../uzi/libraries']);
 
 function createWindow () {
   // Create the browser window.
@@ -17,32 +17,29 @@ function createWindow () {
   win.loadFile('../../gui/ide/index.html');
   win.maximize();
 
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
   win.show();
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    //bat.kill();
-    app.quit()
+    server.kill();
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+});
