9298cf66767f4b3ce5e52628a9f05527c6297af6
RichoM <richi.moran@gmail.com>
2020-10-15 22:20:58 -0300

    Added a hack to change the first item in the sidebar for a button

--- docs/_includes/assets/custom.js
+++ docs/_includes/assets/custom.js
@@ -1,1 +1,21 @@
-console.log("RICHO!");
+var btn = $("<div>")
+	.css("margin", "10px 15px")
+	.append($("<a>")
+		.addClass("btn")
+		.addClass("btn-large")
+		.addClass("btn-block")
+		.css("color", "black")
+		.attr("href", "/PhysicalBits/DOWNLOAD.html")
+		.attr("type", "button")
+		.append($("<i>")
+			.addClass("fa")
+			.addClass("fa-download")
+			.addClass("mx-2"))
+		.append($("<span>")
+			.addClass("text-uppercase")
+			.text("Download")));
+
+var ul = $(".toctree :first-child").first();
+if (ul.get(0).className.search("current") == -1) {
+	ul.replaceWith(btn);
+}
