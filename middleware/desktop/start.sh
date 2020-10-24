705d5d820a33a7b5e02f1a98a9acd5dc2771dfcf
RichoM <richi.moran@gmail.com>
2020-10-24 14:23:40 -0300

    Fixed the start scripts in the desktop app.
    This is a problem because this scripts are dependent on the actual name of the jar file produced by leiningen, and that file name depends on the project version. I don't know how to fix this yet.

--- middleware/desktop/start.sh
+++ middleware/desktop/start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar ../server/target/uberjar/middleware-0.3.0-SNAPSHOT-standalone.jar -w ../../gui -u ../../uzi/libraries
+java -jar ../server/target/uberjar/middleware-1.0.0-SNAPSHOT-standalone.jar -w ../../gui -u ../../uzi/libraries
