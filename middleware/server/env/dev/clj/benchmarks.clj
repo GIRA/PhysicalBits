b85b542a358359e9501ecc34b4a222e35da5262e
RichoM <richi.moran@gmail.com>
2021-04-04 20:49:10 -0300

    Removed unnecessary benchmarking code

--- middleware/server/env/dev/clj/benchmarks.clj
+++ middleware/server/env/dev/clj/benchmarks.clj
@@ -17,56 +17,6 @@
  (with-progress-reporting (bench (new-parser/parse src) :verbose))
 
 
-(def size 100000)
-(defn version-1 [size]
-  (let [result (atom [])]
-    (doseq [n (range 0 size)]
-      (swap! result conj n))
-    @result))
-
- (defn version-2 [size]
-   (let [result (atom (transient []))]
-     (doseq [n (range 0 size)]
-       (swap! result conj! n))
-     (persistent! @result)))
-
- (defn version-3 [size]
-   (let [result (atom (transient []))]
-     (doseq [n (range 0 size)]
-       (reset! result (conj! @result n)))
-     (persistent! @result)))
-
- (defn version-4 [size]
-   (let [result (volatile! (transient []))]
-     (doseq [n (range 0 size)]
-       (vswap! result conj! n))
-     (persistent! @result)))
-
- (defn version-5 [size]
-   (let [result (volatile! (transient []))]
-     (doseq [n (range 0 size)]
-       (vreset! result (conj! @result n)))
-     (persistent! @result)))
-
- (with-progress-reporting (bench (version-1 size) :verbose))
- (with-progress-reporting (bench (version-2 size) :verbose))
- (with-progress-reporting (bench (version-3 size) :verbose))
- (with-progress-reporting (bench (version-4 size) :verbose))
- (with-progress-reporting (bench (version-5 size) :verbose))
-
- (defn vrange [^long n]
-   (loop [i 0 v []]
-     (if (< i n)
-       (recur (inc i) (conj v i))
-       v)))
-
- (defn vrange2 [^long n]
-   (loop [i 0 v (transient [])]
-     (if (< i n)
-       (recur (inc i) (conj! v i))
-       (persistent! v))))
-
-(with-progress-reporting (bench (vrange size) :verbose))
-(with-progress-reporting (bench (vrange2 size) :verbose))
-
+ (def src (slurp "../../uzi/libraries/DCMotor.uzi"))
+ (with-progress-reporting (bench (new-parser/parse src) :verbose))
  ,)
