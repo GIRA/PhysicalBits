bcd1a5c3011ff8c6613adf1a12aeddfe2c458fe3
RichoM <richi.moran@gmail.com>
2021-04-22 09:57:27 -0300

    Testing petitparser from cljs

--- middleware/server/src-cljs/prueba/core.cljs
+++ middleware/server/src-cljs/prueba/core.cljs
@@ -1,16 +1,19 @@
 (ns prueba.core
-  #_(:require [petitparser.core :as pp]))
+  (:require [petitparser.core :as pp]))
 
 (defn foo [] 42)
 
 (defn init []
   (println "Richo capo!!!"))
 
-
-(comment
-(js/alert "Prueba")
 (def parser (pp/plus pp/digit))
 
 (defn parse [str]
   (clj->js (pp/parse parser str)))
+
+(comment
+(js/alert "Prueba")
+
+(parse "45")
+
 ,)
