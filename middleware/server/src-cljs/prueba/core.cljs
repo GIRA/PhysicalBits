fb1a15fbc1b943802a5217bb3104848da1da0e75
RichoM <richi.moran@gmail.com>
2021-04-26 15:38:08 -0300

    Changed the prueba.core to export the parse function and also allow compiling json programs

--- middleware/server/src-cljs/prueba/core.cljs
+++ middleware/server/src-cljs/prueba/core.cljs
@@ -1,29 +1,15 @@
 (ns prueba.core
-  (:require [petitparser.core :as pp])
-  (:require [middleware.parser.ast-nodes :as ast])
   (:require [middleware.parser.parser :as p])
   (:require [middleware.compiler.compiler :as c]))
 
-(defn foo [] 42)
-
-
 (defn init []
-  (println "Richo capo!!!"))
+  (println "Richo capo!"))
 
-(def parser (pp/plus pp/digit))
+(defn ^:export parse [str]
+  (clj->js (p/parse str)))
 
-(defn parse [str]
-  (clj->js (pp/parse parser str)))
-
-(defn ^:export compile [str]
-  (clj->js (c/compile-uzi-string str)))
-
-(comment
-(js/alert "Prueba")
-
-(p/parse "task blink13() running 1/s { toggle(D13); }")
-
-(ast/primitive-node "+" "add")
-(parse "45")
-
-,)
+(defn ^:export compile [str type]
+ (println type)
+ (if (= type "uzi")
+  (clj->js (c/compile-uzi-string str))
+  (clj->js (c/compile-json-string str))))
