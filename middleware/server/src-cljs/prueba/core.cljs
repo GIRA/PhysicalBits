56a33b675d4d837d2a5f01fd8e9166879e436323
RichoM <richi.moran@gmail.com>
2021-04-26 14:46:03 -0300

    Changed prueba.core namespace to export a compile function

--- middleware/server/src-cljs/prueba/core.cljs
+++ middleware/server/src-cljs/prueba/core.cljs
@@ -1,7 +1,8 @@
 (ns prueba.core
   (:require [petitparser.core :as pp])
   (:require [middleware.parser.ast-nodes :as ast])
-  (:require [middleware.parser.parser :as p]))
+  (:require [middleware.parser.parser :as p])
+  (:require [middleware.compiler.compiler :as c]))
 
 (defn foo [] 42)
 
@@ -13,6 +14,9 @@
 
 (defn parse [str]
   (clj->js (pp/parse parser str)))
+
+(defn ^:export compile [str]
+  (clj->js (c/compile-uzi-string str)))
 
 (comment
 (js/alert "Prueba")
