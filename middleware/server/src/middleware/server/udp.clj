23d9f238267111d7f9d37274c8edb0f1f9c2caf3
RichoM <richi.moran@gmail.com>
2021-06-16 20:03:33 -0300

    Added a simple way for the UDP server to receive messages from the client.
    For now, the only available actions it can perform is set either a pin or a global value.

--- middleware/server/src/middleware/server/udp.clj
+++ middleware/server/src/middleware/server/udp.clj
@@ -1,5 +1,6 @@
 (ns middleware.server.udp
   (:require [clojure.core.async :as a :refer [thread <!!]]
+            [clojure.tools.logging :as log]
             [middleware.utils.json :as json]
             [middleware.device.controller :as device]
             [middleware.config :as config])
@@ -8,8 +9,8 @@
 (def server (atom nil))
 
 ; TODO(Richo): Hardcoded single client. Add support for multiple clients
-(def ^:private SERVER_PORT 3232)
-(def ^:private CLIENT_PORT 3234)
+(def ^:const SERVER_PORT 3232)
+(def ^:const CLIENT_PORT 3234)
 
 (def ^:private update-loop? (atom false))
 
@@ -35,26 +36,52 @@
                                                           (:name global)))
                                   (-> state :globals :data vals))}}))
 
+(defn- start-incoming-thread []
+  (thread
+   (let [buf (byte-array 256)
+         packet (DatagramPacket. buf (count buf))]
+     (loop []
+       (when @update-loop?
+         (when-let [^DatagramSocket udp @server]
+           (.receive udp packet)
+           (let [msg (String. buf 0 (.getLength packet) "utf8")
+                 {:keys [action name value]} (json/decode msg)]
+             (println ">>>" msg)
+             (try
+               (case action
+                 "set_pin_value" (device/set-pin-value name value)
+                 "set_global_value" (device/set-global-value name value))
+               (catch Throwable e (log/error "ERROR WHILE RECEIVING (udp) ->" e)))))
+         (recur))))))
+
+
+(defn- start-outgoing-thread []
+  (thread
+   (loop [old-state nil]
+     (when @update-loop?
+       (let [new-state (get-server-state)]
+         (when (not= old-state new-state)
+           (let [^String json-state (json/encode new-state)]
+             (when-let [^DatagramSocket udp @server]
+               (let [buf (.getBytes json-state "utf8")
+                     packet (DatagramPacket. buf
+                                             (count buf)
+                                             (InetAddress/getByName "localhost")
+                                             CLIENT_PORT)]
+                 (.send udp packet)))))
+         (<!! (a/timeout (config/get-in [:udp :interval] 10)))
+         (recur new-state))))))
+
 (defn start-update-loop []
   (when (compare-and-set! update-loop? false true)
-    (thread
-     (loop [old-state nil]
-       (when @update-loop?
-         (let [new-state (get-server-state)]
-           (when (not= old-state new-state)
-             (let [^String json-state (json/encode new-state)]
-               (when-let [^DatagramSocket udp @server]
-                 (.send udp
-                        (DatagramPacket. (.getBytes json-state)
-                                         (.length json-state)
-                                         (InetAddress/getByName "localhost")
-                                         CLIENT_PORT)))))
-           (<!! (a/timeout (config/get-in [:udp :interval] 10)))
-           (recur new-state)))))))
+    (start-incoming-thread)
+    (start-outgoing-thread)))
 
 (comment
-  (stop-update-loop)
-  (start-update-loop)
+  (stop)
+  (start)
+
+  @server
  ,)
 
 (defn stop-update-loop []
