32e0820ad8841688f1178278a5fd5af468e35fd8
RichoM <richi.moran@gmail.com>
2021-08-13 17:16:06 -0300

    Added encode function to core.cljs

--- middleware/server/src/middleware/core.cljs
+++ middleware/server/src/middleware/core.cljs
@@ -1,6 +1,7 @@
 (ns middleware.core
-  (:require [middleware.parser.parser :as p])
-  (:require [middleware.compiler.compiler :as c]))
+  (:require [middleware.parser.parser :as p]
+            [middleware.compiler.compiler :as c]
+            [middleware.compiler.encoder :as en]))
 
 (defn init []
   (println "Richo capo!"))
@@ -13,3 +14,6 @@
   (if (= type "uzi")
     (clj->js (c/compile-uzi-string str))
     (clj->js (c/compile-json-string str))))
+
+(defn ^:export encode [program]
+  (clj->js (en/encode (js->clj program :keywordize-keys true))))
