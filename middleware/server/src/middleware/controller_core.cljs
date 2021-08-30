f0c264c368e8f7ec3a57e547873de237d348b121
RichoM <richi.moran@gmail.com>
2021-08-30 19:53:38 -0300

    Changed main function to provide a minimal interactive console to test the controller

--- middleware/server/src/middleware/controller_core.cljs
+++ middleware/server/src/middleware/controller_core.cljs
@@ -1,41 +1,37 @@
 (ns middleware.controller-core
-  (:require ;["serialport" :as SerialPort]
-            [clojure.core.async :as a]
+  (:require ["readline" :as readline]
+            [clojure.core.async :as a :refer [go go-loop <! timeout]]
+            [middleware.device.controller :as dc]
             [middleware.device.ports.common :as ports]
             [middleware.device.ports.serial :as serial]
             [middleware.parser.parser :as p]
             [middleware.compiler.compiler :as c]
             [middleware.compiler.encoder :as en]))
 
+(def rl (.createInterface readline
+                          #js {:input js/process.stdin
+                               :output js/process.stdout}))
+
+(enable-console-print!)
+
+(defn question [q]
+  (let [c (a/promise-chan)]
+    (.question rl q (fn [d] (a/put! c d)))
+    c))
+
 (defn main []
   (ports/register-constructors! #'serial/open-port)
-  (println "Richo capo!"))
-
-(comment
-
-@ports/constructors
-
-(def port (ports/connect! "COM4" 9600))
-port
-(ports/disconnect! port)
-(a/go-loop [i 0]
-  (when-let [data (a/<! (:in port))]
-    (println i data)
-    (recur (inc i))))
-
-(a/put! (:out port) [65 66 67])
-
-
-(def port (SerialPort. "COM4" {:baudRate 9600}))
-
-(.on port "data" (fn [data]
-                   (dotimes [i (.-length data)]
-                            (println (aget data i)))))
-(js/Uint8Array.from [0 1 2 3 4 5])
-(.write port "Richo!")
-(.write port (js/Uint8Array.from [65 66 67]))
-(.close port)
-(c/compile-uzi-string "task blink13() running 1/s { toggle(D13); }")
-
-SerialPort
- ,,,)
+  (println "Richo capo!")
+  (go
+   (<! (timeout 500))
+   (let [port-name (<! (question "Ingrese el puerto: "))]
+     (dc/connect! port-name))
+   (loop []
+     (println)
+     (try
+       (let [src (<! (question "uzi> "))
+             program (c/compile-uzi-string src)]
+         (dc/run program))
+       (catch :default ex
+         (println "ERROR" ex)))
+     (recur))))
