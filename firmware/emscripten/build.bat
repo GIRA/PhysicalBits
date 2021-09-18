50406f070793e91c7b181bc198b3ebcdfd3f70c8
RichoM <richi.moran@gmail.com>
2021-09-18 17:21:31 -0300

    Updated build.bat to include all uzi source files.

--- firmware/emscripten/build.bat
+++ firmware/emscripten/build.bat
@@ -1,8 +1,14 @@
 @echo off
 set sim=../Simulator/Sketch
-set src=main.c %sim%/Arduino.cpp %sim%/EEPROM.cpp %sim%/Servo.cpp %sim%/Simulator.cpp
+set sim_files=%sim%/Arduino.cpp %sim%/EEPROM.cpp %sim%/Servo.cpp %sim%/Simulator.cpp
+set uzi=../UziFirmware
+set uzi_files=%uzi%/ArrayReader.cpp %uzi%/Coroutine.cpp %uzi%/EEPROMReader.cpp %uzi%/EEPROMWearLevelingReader.cpp %uzi%/EEPROMWearLevelingWriter.cpp^
+ %uzi%/EEPROMWriter.cpp %uzi%/GPIO.cpp %uzi%/Instruction.cpp %uzi%/Memory.cpp %uzi%/Monitor.cpp %uzi%/Program.cpp %uzi%/Reader.cpp %uzi%/Script.cpp^
+ %uzi%/SerialReader.cpp %uzi%/UziSerial.cpp %uzi%/VM.cpp -x c++ %uzi%/UziFirmware.ino
 
-set opts=-I %sim% -fdeclspec -s LLD_REPORT_UNDEFINED
+set src=main.c %sim_files% %uzi_files%
+
+set opts=-I %sim% -I %uzi% -fdeclspec -s LLD_REPORT_UNDEFINED
 
 mkdir out 2> NUL
 em++ %opts% %src% -o out/main.js && node out/main.js
