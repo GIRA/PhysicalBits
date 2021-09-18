9578a5fc211df718e88e5a9d2c1fef7e16eb6e52
RichoM <richi.moran@gmail.com>
2021-09-18 15:41:56 -0300

    Testing some basic stuff

--- firmware/emscripten/main.c
+++ firmware/emscripten/main.c
@@ -7,9 +7,45 @@
 
 #include <stdio.h>
 #include "Arduino.h"
+#include "Simulator.h"
 
 void setup() {
-  pinMode(13, OUTPUT);
+  printf("_____________");
+  printf("Testing basic GPIO\n");
+  {
+    pinMode(13, OUTPUT);
+    digitalWrite(13, HIGH);
+    int val = digitalRead(13);
+    printf("PIN 13: %d\n", val);
+    digitalWrite(13, LOW);
+    val = digitalRead(13);
+    printf("PIN 13: %d\n", val);
+  }
+
+  printf("_____________");
+  printf("Testing serial\n");
+  {
+    Serial.begin(9600);
+
+    Serial_write((char*)"RICHO", 5);
+
+    printf("AVAILABLE: %d\n", Serial.available());
+    while (Serial.available()) {
+      char val = Serial.read();
+      printf("READ: %d\n", val);
+    }
+
+    Serial.write(42);
+    Serial.write(43);
+    Serial.write(44);
+    Serial.write(45);
+
+    char buf[10];
+    int count = Serial_readInto(buf, sizeof(buf));
+    for (int i = 0; i < count; i++) {
+      printf("WRITE: %d\n", buf[i]);
+    }
+  }
 }
 
 void loop() {
