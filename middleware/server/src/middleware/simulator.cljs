62be54db493d76adc0511e766849a4be26f77678
RichoM <richi.moran@gmail.com>
2021-09-25 17:47:40 -0300

    Added a "simulator" build.
    This will contain the controller compiled for the browser and configured to use a fake serial that would communicate with the wasm firmware.

---
+++ middleware/server/src/middleware/simulator.cljs
@@ -0,0 +1,19 @@
+(ns middleware.simulator
+  (:require [clojure.core.async :as a :refer [go go-loop <! timeout]]
+            [middleware.device.controller :as dc]
+            [middleware.device.ports.common :as ports]
+            ;[middleware.device.ports.serial :as serial]
+            [middleware.compiler.compiler :as cc]
+            [middleware.utils.fs.common :as fs]
+            [middleware.utils.fs.browser :as browser]))
+
+(defn init-dependencies []
+  (fs/register-fs! #'browser/file)
+  #_(ports/register-constructors! #'serial/open-port))
+
+(defn init []
+  (init-dependencies)
+  (println "Richo capo!"))
+
+(defn chan->promise [ch]
+  (js/Promise. (fn [res] (a/take! ch res))))
