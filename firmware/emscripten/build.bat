11adf762cf0d37e2ca4320b5af039dcb590e9528
RichoM <richi.moran@gmail.com>
2021-09-29 10:06:12 -0300

    Changed build.bat to output files in the /gui/ide/ folder

--- firmware/emscripten/build.bat
+++ firmware/emscripten/build.bat
@@ -12,7 +12,8 @@ set opts=-I %sim% -I %uzi% -fdeclspec -s LLD_REPORT_UNDEFINED
 
 set exports=_GPIO_getPinValue,_GPIO_setPinValue,_Sketch_setup,_Sketch_loop,_Serial_readInto,_Serial_write,_Sketch_getMillis,_Sketch_setMillis,_EEPROM_read,_EEPROM_write,_EEPROM_size,_Stats_usedMemory,_Stats_coroutineResizeCounter
 
-mkdir out 2> NUL
-REM em++ %opts% %src% -o out/main.js && node out/main.js
+REM mkdir out 2> NUL && set out=out/simulator.js
+set out=../../gui/ide/simulator.js
+
 @echo on
-em++ %opts% %src% -o out/simulator.html -s EXPORTED_FUNCTIONS=%exports%,_main,_malloc,_free -s EXPORTED_RUNTIME_METHODS=ccall,cwrap --post-js bindings.js
+em++ %opts% %src% -o %out% -s EXPORTED_FUNCTIONS=%exports%,_main,_malloc,_free -s EXPORTED_RUNTIME_METHODS=ccall,cwrap --post-js bindings.js
