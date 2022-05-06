a1e2259b9cfd847d33e93bf82bed62b2c72b4e55
RichoM <richi.moran@gmail.com>
2022-05-06 11:36:06 -0300

    Added basic csv writing to the eventlog

--- middleware/server/src/middleware/utils/eventlog.clj
+++ middleware/server/src/middleware/utils/eventlog.clj
@@ -1,15 +1,28 @@
 (ns middleware.utils.eventlog
   (:require [clojure.java.io :as io]
+            [clojure.data.csv :as csv]
             [clojure.string :as str]
-            [middleware.utils.core :refer [parse-int]]))
+            [middleware.utils.core :refer [parse-int random-uuid]]))
 
-(def ^:const FILE_SIZE_LIMIT (* 1024 1024))
+(def pc-id (try
+             (slurp "pc.uuid")
+             (catch java.lang.Throwable _
+               (let [id (random-uuid)]
+                 (try (spit "pc.uuid" id)
+                      (catch java.lang.Throwable _))
+                 (str id)))))
+
+(def session-id (str (random-uuid)))
+
+(def FILE_SIZE_LIMIT (* 100 1024 1024))
 
 (def file-name (atom nil))
 
 (defn today []
   (java.time.LocalDate/now))
 
+(defn now []
+  (str (java.time.Instant/now)))
 
 (defn events-folder []
   (io/file "events"))
@@ -39,21 +52,31 @@
          (inc-counter last)
          (str date ".1.csv"))))))
 
-(defn append [str]
-  (spit (io/file "events" (find-log-file))
-        str :append true))
+(defn append [evt-type evt-data]
+  (try
+    (let [file (io/file "events" (find-log-file))
+          data [pc-id session-id (now) evt-type evt-data]]
+      ; TODO(Richo): Optimization. Reuse the writer until file changes
+      (with-open [writer (io/writer file :append true)]
+        (csv/write-csv writer [data])))
+    (catch java.lang.Throwable _)))
 
 
 (comment
 
+  (def w (io/writer "foo.txt" :append true))
+  (time (csv/write-csv w [[pc-id session-id (now) "RICHO" {:a 1 :b 2 :c "Richo, capo"}]]))
+
+  (.close w)
+  
+  (time (append "RICHO" {:a 1 :b 2 :c "Richo, capo"}))
 
   (require '[clojure.core.async :as a :refer [<! go]])
 
   (def stop (atom false))
-
   (go (loop [i 0]
         (when-not @stop
-          (append (str "RICHO " i "\n"))
+          (append "RICHO" i)
           (when (= 0 (mod i 1000))
             (<! (a/timeout 10)))
           (recur (inc i))))
