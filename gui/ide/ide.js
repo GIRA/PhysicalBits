9a64b8b9432808b0762c66055c57b3b373c18b1d
RichoM <richi.moran@gmail.com>
2023-05-15 18:37:45 -0300

    Fixed default cols/rows for the LCD modal

--- gui/ide/ide.js
+++ gui/ide/ide.js
@@ -277,7 +277,7 @@ let IDE = (function () {
       let spec = {
         title: i18n.translate("Configure lcds"),
         cantRemoveMsg: i18n.translate("This lcd is being used by the program!"),
-        defaultElement: { name: "lcd", address: 63, cols: 0, rows: 0 },
+        defaultElement: { name: "lcd", address: 63, cols: 16, rows: 2 },
         columns: [
           {id: "name", type: "identifier", name: i18n.translate("Lcd name")},
           {id: "address", type: "number", name: i18n.translate("Address")},
